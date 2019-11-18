91b0edad8c56141929249653be1956124d351e88
Brian Chief <brian@jobsikaz.com>
2019-11-18 10:09:19 +0300

    blogs slug error

--- database/migrations/2019_10_03_083838_create_blogs_table.php
+++ database/migrations/2019_10_03_083838_create_blogs_table.php
@@ -14,7 +14,7 @@ class CreateBlogsTable extends Migration
             $table->integer('user_id');
             $table->integer('blog_category_id');
             $table->string('title',500);
-            $table->string('slug',800)->unique();
+            $table->string('slug',570)->unique();
             $table->text('contents');
             $table->string('image1',500)->nullable();
             $table->string('image2',500)->nullable();
