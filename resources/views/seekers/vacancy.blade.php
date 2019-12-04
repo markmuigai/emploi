755147a8be679b069c5b7fec66be9bb2aedd5e9d
Brian Chief <brian@jobsikaz.com>
2019-12-04 15:42:56 +0300

    posts meta

--- resources/views/seekers/vacancy.blade.php
+++ resources/views/seekers/vacancy.blade.php
@@ -3,7 +3,7 @@
 @section('title','Emploi :: '.$post->title )
 
 @section('description')
-Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs in the job marketplace.
+{{ $post->brief }}
 @endsection
 
 @section('content')
