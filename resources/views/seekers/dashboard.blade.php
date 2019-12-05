0660600984b8276a8ab7f34e2966af3bd3355fa9
Brian Chief <brian@jobsikaz.com>
2019-12-05 14:26:26 +0300

    job seeker recent applications error

--- resources/views/seekers/dashboard.blade.php
+++ resources/views/seekers/dashboard.blade.php
@@ -9,46 +9,6 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
 @section('content')
 @section('page_title', 'Dashboard')
 
-<h4>Recent Applications</h4>
-<div class="row">
-	@forelse($posts as $p)
-	<div class="col-lg-6">
-		<div class="card my-3">
-			<div class="card-body">
-				<h4>{{ $p->title }}</h4>
-				<h6 class="orange">
-					<a href="/companies/{{ $p->company->name }}">
-						{{ $p->company->name }}
-					</a>
-				</h6>
-				<p><strong>Applied on: </strong>{{ $p->created_at }}</p>
-				<div class="row align-items-center">
-					<div class="col-6">
-						<p>{{ $p->monthlySalary() }}</p>
-						@if($p->post->isShortlisted($user->seeker))
-						<p class="text-success">Shortlisted</p>
-						@else
-						<p class="text-primary">Pending</p>
-						@endif
-					</div>
-					<div class="col-6">
-						<a href="/profile/applications/{{ $p->id }}" class="btn btn-orange pull-right">View</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	@empty
-	<div class="col-12 my-3 text-center">
-		<div class="card">
-			<div class="card-body">
-				<p>You haven't applied to any jobs yet.</p>
-				<a href="/vacancies" class="btn btn-orange">Apply Now</a>
-			</div>
-		</div>
-	</div>
-	@endforelse
-</div>
 
 <h4>Recent Blogs</h4>
 <div class="row">
