85cbef8be433fd67afce17f8a6b8326869f3e01e
Brian Chief <brian@jobsikaz.com>
2019-12-05 10:08:21 +0300

    footer minus posts

---
+++ resources/views/components/top-search.blade.php
@@ -0,0 +1,47 @@
+<div class="top-search py-4">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 col-lg-3">
+                <h5>Top Industries</h5>
+                <ul>
+                    @forelse(\App\Industry::top(10) as $f)
+                    <li><a href="/vacancies/{{ $f->slug }}">{{ $f->name }}</a></li>
+                    @empty
+                    @endforelse
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <h5>Jobs by Category</h5>
+                <ul>
+                    <li>
+                        <a href="/vacancies/featured">Featured</a>
+                    </li>
+                    @forelse(\App\VacancyType::all() as $v)
+                    <li>
+                        <a href="/vacancies/{{ $v->slug }}">{{ $v->name }}</a>
+                    </li>
+                    @empty
+                    @endforelse
+                    <li>
+                        <a href="/vacancies">All Jobs</a>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <h5>Hiring Companies</h5>
+                <ul>
+                    @forelse(\App\Company::getHiringCompanies() as $v)
+                    <li>
+                        <a href="/companies/{{ $v->name }}">{{ $v->name }}</a>
+                    </li>
+                    @empty
+                    @endforelse
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
