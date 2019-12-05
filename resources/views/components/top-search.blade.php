53c38a8eec2b82366a6f7f7aaf4f42180d53e4fb
Brian Chief <brian@jobsikaz.com>
2019-12-05 11:13:42 +0300

    disabled hiring companies

--- resources/views/components/top-search.blade.php
+++ resources/views/components/top-search.blade.php
@@ -1,7 +1,7 @@
 <div class="top-search py-4">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-6 col-lg-3">
+            <div class="col-12 col-md-6 col-lg-4">
                 <h5>Top Industries</h5>
                 <ul>
                     @forelse(\App\Industry::top(10) as $f)
@@ -10,36 +10,65 @@
                     @endforelse
                 </ul>
             </div>
-            <div class="col-12 col-md-6 col-lg-3">
-                
+            <div class="col-12 col-md-6 col-lg-4">
+                <?php
+                    $featured = \App\Post::featured(10);
+                    $missing = 10 - count($featured);
+                ?>
+                <h5>Featured Jobs</h5>
+                <ul>
+                    @forelse($featured as $f)
+                    <li><a href="/vacancies/{{ $f->slug }}">{{ $f->title }}</a></li>
+                    @empty
+                    @endforelse
+                    @if($missing > 0)
+                        @forelse(\App\Post::recent($missing) as $f)
+                        <li><a href="/vacancies/{{ $f->slug }}">{{ $f->title }}</a></li>
+                        @empty
+                        @endforelse
+
+                    @endif
+                </ul>
             </div>
-            <div class="col-12 col-md-6 col-lg-3">
+            <div class="col-12 col-md-6 col-lg-4">
                 <h5>Jobs by Category</h5>
                 <ul>
                     <li>
                         <a href="/vacancies/featured">Featured</a>
                     </li>
-                    @forelse(\App\VacancyType::all() as $v)
                     <li>
-                        <a href="/vacancies/{{ $v->slug }}">{{ $v->name }}</a>
+                        <a href="/vacancies/full-time">Full Time</a>
                     </li>
-                    @empty
-                    @endforelse
+                    <li>
+                        <a href="/vacancies/part-time">Part-time</a>
+                    </li>
+                    <li>
+                        <a href="/vacancies/internships">Internships</a>
+                    </li>
+                    <li>
+                        <a href="/vacancies/contract">Contract</a>
+                    </li>
+                    <li>
+                        <a href="/vacancies/volunteer">Volunteer</a>
+                    </li>
+                    <li>
+                        <a href="/vacancies/remote">Remote</a>
+                    </li>
                     <li>
                         <a href="/vacancies">All Jobs</a>
                     </li>
 
                 </ul>
             </div>
-            <div class="col-12 col-md-6 col-lg-3">
+            <div class="col-12 col-md-6 col-lg-3" style="display: none">
                 <h5>Hiring Companies</h5>
                 <ul>
-                    @forelse(\App\Company::getHiringCompanies() as $v)
+                    {{-- @forelse(\App\Company::getHiringCompanies() as $v)
                     <li>
                         <a href="/companies/{{ $v->name }}">{{ $v->name }}</a>
                     </li>
                     @empty
-                    @endforelse
+                    @endforelse --}}
                 </ul>
             </div>
         </div>
