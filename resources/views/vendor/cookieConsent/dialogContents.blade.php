b731f411fd1b900d169be5f8dbf2ad689b61da2d
LatashaWanjira <latasha.wanjira@gmail.com>
2019-12-09 15:28:05 +0300

    small changes on cookie

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,10 +1,10 @@
-<div class="js-cookie-consent cookie-consent" style="position: fixed; bottom: 0; z-index: 300; width: 100%; text-align: center; background-color: #e88725; color: white">
+<div class="js-cookie-consent cookie-consent" style="position: fixed; bottom: 0; z-index: 300; width: 100%; text-align: center; background-color: #500095; color: white" padding="0.5em 0">
 
     <span class="cookie-consent__message">
         {!! trans('cookieConsent::texts.message') !!}
     </span>
 
-    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-primary">
+    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-orange">
         {{ trans('cookieConsent::texts.agree') }}
     </button>
 
