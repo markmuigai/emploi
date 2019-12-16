697dd6fddcd3c82b6895c8844e6b82f3d2b26aae
Brian Chief <brian@jobsikaz.com>
2019-12-16 09:54:54 +0300

    split posts command

--- app/Console/Commands/SplitPosts.php
+++ app/Console/Commands/SplitPosts.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-use App\Blog;
+use App\Post;
 
 class SplitPosts extends Command
 {
@@ -42,26 +42,37 @@ class SplitPosts extends Command
     {
         $this->info("Starting to Split Imported Posts that have how to apply");
 
-        $sql = "SELECT id, responsibilities FROM posts WHERE responsibilities LIKE '%<strong>how to apply</strong>%'";
+        $sql = "SELECT id,responsibilities FROM posts WHERE responsibilities LIKE '%<strong>how to apply</strong>%'";
         $results = DB::select($sql);
-        // for($i=0; $i<count($results); $i++)
-        // {
-        //     $post = Post::findOrFail($results[$i]->id);
-        //     $responsibilities =
-        // }
-        // dd(count($results));
+        $changed =0;
+        for($i=0; $i<count($results); $i++)
+        {
+            $post = Post::findOrFail($results[$i]->id);
 
-        // dd($results[rand(0,count($results)-1)]->responsibilities);
+            $tit = strtolower($post->title);
+            if(strpos($tit, 'how to apply'))
+                continue;
 
-        dd(htmlspecialchars($results[rand(0,count($results)-1)]->responsibilities));
+            $hay = strtolower($post->responsibilities);
 
-        $post = explode('<strong>how to apply</strong>', $results[rand(0,count($results)-1)]->responsibilities);
-        //$post = explode($results[rand(0,count($results)-1)]->responsibilities, );
-        dd($post);
-        print count($post);
-        print $post[0];
-        dd($post[1]);
-        dd($post[1]);
-        dd(count($results));
+            $respo = $post->responsibilities;
+            $apply = '';
+
+            $startPos = strpos($hay, '<strong>how to apply</strong>');
+            $apply = substr($respo, $startPos);
+            $respo = substr($respo, 0,$startPos);
+
+            $post->how_to_apply = substr($respo, $startPos);
+            $post->responsibilities = substr($respo, 0,$startPos);;
+
+            $post->save();
+
+            $changed++;
+
+            $this->info($post->title . " changed");
+        }
+
+        $this->info($changed . " posts changed");
+        
     }
 }
