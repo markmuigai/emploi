0d8dc708e341bf0f03ca7492c0858ac8602a09ae
Obare C. Brian <brian@emploi.co>
2019-12-24 11:48:22 +0300

    save referee assessmenet remove start date

--- resources/views/employers/rsi/referees.blade.php
+++ resources/views/employers/rsi/referees.blade.php
@@ -20,7 +20,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
         <div class="row">
             @forelse($app->seekerApplications as $b)
             <div class="col-md-4 col-6">
-                {{ dd($b->jobApplicationReferee) }}
+                {{ dd($b) }}
                 {{ $b->jobApplicationReferee->referee->relationship }} at {{ $b->jobApplicationReferee->referee->organization }}
                 <strong>{{ $b->jobApplicationReferee->referee->name }}</strong>
             </div>
