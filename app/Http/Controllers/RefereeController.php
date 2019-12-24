ca2f23d3e494bc45c8b0dcfdf3b604bd56a6e451
Obare C. Brian <brian@emploi.co>
2019-12-24 11:40:26 +0300

    save referee assessmenet remove start date

--- app/Http/Controllers/RefereeController.php
+++ app/Http/Controllers/RefereeController.php
@@ -62,7 +62,7 @@ class RefereeController extends Controller
     	if($r->ready)
     	{
     		$j = $r->jobApplicationReferee;
-    		$j->start_date = $request->start_date;
+    		//$j->start_date = $request->start_date;
     		$j->relationship = $request->relationship;
     		$j->reason_for_leaving = $request->reason_for_leaving;
     		$j->performance = $request->performance;
