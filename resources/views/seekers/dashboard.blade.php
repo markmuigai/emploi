c241610e365f4f6cdfa221355c0009a98d4055e4
Obare C. Brian <brian@emploi.co>
2019-12-31 12:04:59 +0300

    job seeker jobs in dashboard

--- resources/views/seekers/dashboard.blade.php
+++ resources/views/seekers/dashboard.blade.php
@@ -44,5 +44,56 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
 	@endforelse
 </div>
 
+<?php
+$user = Auth::user();
+?>
+
+@if(isset($user->seeker->industry_id))
+<hr>
+<h4>{{ $user->seeker->industry->name }} Jobs</h4>
+<?php
+$posts = \App\Post::where('industry_id',$user->seeker->industry_id)->where('status','active')->orderBy('id','DESC')->orderBy('featured','DESC')->paginate(2);
+?>
+<div class="row">
+	
+	@forelse ($posts as $b)
+	<div class="col-lg-6">
+		<div class="card my-2">
+			<div class="card-body">
+				<div class="row align-items-center">
+					<div class="col-4">
+						<img src="{{ asset($b->imageUrl) }}" class="w-100" alt="{{ $b->title }}">
+					</div>
+					<div class="col-8">
+						<h5>{{ $b->title }}</h5>
+						<span class="badge badge-orange">
+							<a href="/vacancies/{{ $b->slug }}">
+								{{ $b->monthlySalary() }}
+							</a>
+						</span>
+						<a href="/vacancies/{{ $b->slug }}" class="btn btn-sm btn-orange-alt pull-right">View and Apply</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	@empty
+	<div class="col-12 my-3 text-center">
+		<div class="card">
+			<div class="card-body">
+				<p>No Industry jobs have been posted.</p>
+			</div>
+		</div>
+	</div>
+	@endforelse
+</div>
+<div>
+	{{ $posts->links() }}
+</div>
+
+@else
+
+@endif
+
 
 @endsection
