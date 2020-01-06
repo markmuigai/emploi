43674263ec007f9ec8d4c0d0ea3c2646b92535f4
Obare C. Brian <brian@emploi.co>
2020-01-06 10:53:26 +0300

    onesignal push

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -68,3 +68,20 @@
 <script data-ad-client="ca-pub-9948474979900683" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@12.4.0/dist/lazyload.min.js"></script>
+
+<!-- ONESIGNAL -->
+@if(Request::server ("SERVER_NAME") == 'emploi.co' || Request::server ("SERVER_NAME") == 'beta.emploi.co')
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  var OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "926b129f-755b-4029-a039-e9ef27e36b16",
+      notifyButton: {
+        enable: true,
+      },
+    });
+  });
+</script>
+
+@endif
