b70f12ff534fd3b95086c561ffb0761c725800c5
Obare C. Brian <brian@emploi.co>
2020-01-07 13:50:01 +0300

    renamed service worker

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -32,7 +32,7 @@
 <script type="text/javascript">
     // Initialize the service worker
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/serviceworker.js', {
+        navigator.serviceWorker.register('/sw.js', {
             scope: '.'
         }).then(function(registration) {
             // Registration was successful
