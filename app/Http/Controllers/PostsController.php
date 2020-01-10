f490e107d685d08f73360f776ccb5f0eac22b271
Obare C. Brian <brian@emploi.co>
2020-01-10 12:35:32 +0300

    changed featured order

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -56,9 +56,8 @@ class PostsController extends Controller
         $query = isset($request->q) ? $request->q : "";
         $posts = Post::whereRaw("UPPER('title') != '". strtoupper('HOW TO APPLY')."'")
             ->where('status','!=','inactive')
-            ->orderBy('created_at','DESC')
             ->orderBy('featured','DESC')
-            
+            ->orderBy('created_at','DESC')
             ->paginate(10)->onEachSide(3);
         return view('seekers.vacancies')
                 ->with('industries',Industry::active())
