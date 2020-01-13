9842006add5e67379d0470167c08f920d44602d5
Obare C. Brian <brian@emploi.co>
2020-01-13 17:10:17 +0300

    admin view profile image

--- resources/views/admins/seekers/index.blade.php
+++ resources/views/admins/seekers/index.blade.php
@@ -69,7 +69,10 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
     <div class="card-body">
         @forelse($seekers as $seeker)
         <div class="row">
-            <div class="col-md-6 col-12">
+            <div class="col-md-3">
+                <img src="{{ asset($seeker->user->getPublicAvatarUrl()) }}" style="width: 100%; border-radius: 50%">
+            </div>
+            <div class="col-md-5 col-12">
                 <h4>
                     <a href="/admin/seekers/{{ $seeker->user->username }}" class="orange">
                         {{ $seeker->user->name }}
@@ -82,7 +85,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                 <p>Registered: {{ $seeker->user->created_at }}</p>
                 <p>Last CV Update: {{ $seeker->user->updated_at }}</p>
             </div>
-            <div class="col-md-6 col-12 text-md-right text-left">
+            <div class="col-md-4 col-12 text-md-right text-left">
                 <p><a href="mailto:{{ $seeker->user->email }}" class="orange">{{ $seeker->user->email }}</a></p>
                 <p>{{ $seeker->phone_number }}</p>
                 <p>{{ $seeker->sex }}</p>
