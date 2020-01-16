1aa9a3628e78cf1c35bfe67b6e3b102e1a3f5a6b
Obare C. Brian <brian@emploi.co>
2020-01-16 17:12:38 +0300

    blog share on home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -346,7 +346,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                         <p class="badge badge-secondary">{{ $blog->category->name }}</p>
                         <p class="truncate-long">{!!html_entity_decode($blog->longPreview(500))!!}</p>
                         <a href="{{ url('blog/'.$blog->slug) }}" class="orange">Read More</a>
-                        <span>
+                        <span style="float: right; text-align: right;">
                             <a href="{{ $blog->shareFacebookLink }}" target="_blank" rel="noreferrer" style="margin-right: 1em"><i class="fab fa-facebook-f"></i></a>
                             <a href="{{ $blog->shareTwitterLink }}" target="_blank" rel="noreferrer" style="margin-right: 1em"><i class="fab fa-twitter"></i></a>
                             <a href="{{ $blog->shareLinkedinLink }}" target="_blank" rel="noreferrer" style="margin-right: 1em"><i class="fab fa-linkedin"></i></a>
