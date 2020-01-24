71b40753ba4471de6377b4b09b023bc74f77aad6
Obare C. Brian <brian@emploi.co>
2020-01-24 15:16:32 +0300

    ads on all pages

--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -169,23 +169,7 @@
                     <!-- ADD JOB AS AN EMPLOYER -->
                     <div id="postJob" class="mb-2">
                         <h2>@yield('page_title')</h2>
-                        <ins class="adsbygoogle"
-
-                             style="display:block"
-
-                             data-ad-client="ca-pub-9948474979900683"
-
-                             data-ad-slot="6102731895"
-
-                             data-ad-format="auto"
-
-                             data-full-width-responsive="true"></ins>
-
-                        <script>
-
-                             (adsbygoogle = window.adsbygoogle || []).push({});
-
-                        </script>
+                        @include('components.ads.responsive')
                         @if( isset(Auth::user()->id) && Auth::user()->role == 'employer' )
                         <a href="/vacancies/create" class="btn btn-orange" dusk="create-new-post"><i class="fas fa-plus"></i> Post A Job</a>
 
