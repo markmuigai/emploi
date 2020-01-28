ec31c78de0038ac8f98079414760c814f0c55ac0
Obare C. Brian <brian@emploi.co>
2020-01-28 11:38:24 +0300

    contact on rate card

--- resources/views/employers/rate-card.blade.php
+++ resources/views/employers/rate-card.blade.php
@@ -25,7 +25,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
 
                 <div class="text-center">
                     <hr>
-                    <a href="#get-package" class="btn btn-orange-alt">Get Package</a>
+                    <a href="/contact" class="btn btn-orange-alt">Get Package</a>
                 </div>
             </div>
         </div>
@@ -44,7 +44,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                 </ul>
                 <div class="text-center">
                     <hr>
-                    <a href="#get-package" class="btn btn-orange-alt">Get Package</a>
+                    <a href="/contact" class="btn btn-orange-alt">Get Package</a>
                 </div>
             </div>
 
@@ -64,7 +64,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                 </ul>
                 <div class="text-center">
                     <hr>
-                    <a href="#get-package" class="btn btn-orange-alt">Get Package</a>
+                    <a href="/contact" class="btn btn-orange-alt">Get Package</a>
                 </div>
             </div>
         </div>
@@ -88,7 +88,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                 </ul>
                 <div class="text-center">
                     <hr>
-                    <a href="#get-package" class="btn btn-orange-alt">Get Package</a>
+                    <a href="/contact" class="btn btn-orange-alt">Get Package</a>
                 </div>
             </div>
         </div>
@@ -122,24 +122,10 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                 
                 <div class="text-center">
                     <hr>
-                    <a href="#get-package" class="btn btn-orange-alt">Get Package</a>
+                    <a href="/contact" class="btn btn-orange-alt">Get Package</a>
                 </div>
             </div>
         </div>
     </div>
-    <div id="get-package" class="text-center mt-5" style="display: none">
-        <div class="card py-5">
-            <div class="card-body d-flex flex-column justify-content-between">
-                <h3 class="orange">Reach out to us today</h3>
-                <h6 class="mt-3">Call Us</h6>
-                <p>
-                    <a href="tel:+254702068282">0702 068 282</a> | <a href="tel:+254774569001">0774 569 001</a> | <a href="tel:+254772795017">0772 795 017</a>
-                </p>
-                <h6 class="mt-3">Find Us</h6>
-                <p>Even Business Park, Airport North Rd, Nairobi</p>
-                <h6 class="mt-3">Send Us An Email</h6>
-                <p><a href="mailto:info@emploi.co">info@emploi.co</a> || <a href="mailto:sales@emploi.co">sales@emploi.co</a></p>
-            </div>
-        </div>
-    </div>
+    
     @endsection
