5c5aa292da8df84c71c531a6b7d6efc29302ccfe
Obare C. Brian <brian@emploi.co>
2020-01-31 15:26:49 +0300

    more login buttons

--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -132,12 +132,16 @@
 
                         <!-- GUEST SIDEBAR -->
                         @else
+                        @include('components.social-auth')
+                        <br>
                         <div class="card">
                             <div class="card-body text-center">
-                                <h5>Sign Up Today</h5>
+                                
                                 <a href="/join" class="btn btn-orange">Register</a>
                                 <h6 class="mt-3">- Have An Account -</h6>
                                 <a href="/login" class="btn btn-orange-alt">Login</a>
+
+
                             </div>
                         </div>
                         <!-- END OF GUEST SIDEBAR -->
