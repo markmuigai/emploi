c28dca39c444e40fb3d6e2c136bedcaab8838e1d
Obare C. Brian <brian@emploi.co>
2020-02-04 14:54:17 +0300

    front page changes

--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -2,17 +2,18 @@
 <h1>Blast Off Your Career</h1>
 <p>{{ $line }}</p>
 <?php
-$subcat = 'featured';
+$subcat = '';
 if(isset(Auth::user()->id))
 {
 	if(isset(Auth::user()->seeker->industry_id))
 		$subcat = Auth::user()->seeker->industry->slug;
+	else
+		$subcat = 'featured';
 }
 
 ?>
 <p style="text-align: center;">
     <a href="/vacancies/{{ $subcat }}" class="btn btn-orange px-4">Latest Vacancies</a>
-    <a href="/job-seekers/services" class="btn btn-white px-4">Services</a>
     <br class="for-mobile"><br class="for-mobile">
-    <a href="/job-seekers/cv-editing#request-cv-edit-form" class="btn btn-success px-4">Request CV Editing</a>
+    <a href="/job-seekers/cv-editing#request-cv-edit-form" class="btn btn-default px-4" style="color: black; background-color: white">Request CV Editing</a>
 </p>
