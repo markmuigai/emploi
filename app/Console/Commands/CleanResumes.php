6be4d4fd4e0661958f27b301fb9dcaf164b8b6a9
Obare C. Brian <brian@emploi.co>
2020-02-04 16:22:45 +0300

    track storage use

---
+++ app/Console/Commands/CleanResumes.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Seeker;
+
+class CleanResumes extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'CleanResumes';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deletes all resumes that are not mentioned on job seekers table.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //return $this->info();
+        $wasted = 0;
+        $useful = 0;
+
+        foreach (glob(storage_path()."/app/public/resumes/*") as $filename) {
+            $name = explode("/", $filename);
+            $name = $name[count($name)-1];
+            $seeker = Seeker::where('resume',$name)->first();
+            if(!isset($seeker->id))
+                $wasted += filesize($filename) / 1024;
+            else
+                $useful += filesize($filename) / 1024;
+            //$this->info($name);
+            //$this->info("$filename size " . filesize($filename));
+        }
+        $this->info("Wasted Storage on CVs: ".round($wasted)." MB");
+        $this->info("Used Storage on CVs: ".round($useful)." MB");
+    }
+}
