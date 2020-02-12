aa6f1346ee88bcfc2ac23eb598604841bd0e9205
Obare C. Brian <brian@emploi.co>
2020-02-12 10:10:41 +0300

    payment middleware

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -14,7 +14,7 @@ class InvoiceController extends Controller
 {
     public function __construct() {
         $this->middleware('admin', ['except' => [
-            'show'
+            'show','payment'
         ]]);
     }   
 
