e1c1f806818476f2ff3c6e4b88f3ce3635aa1d91
kirarit <davidkirarit@gmail.com>
2020-02-27 09:07:25 +0300

    slight error fix

--- resources/views/admins/seekers/index.blade.php
+++ resources/views/admins/seekers/index.blade.php
@@ -91,7 +91,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                 </h4>
                 <p>{{ $seeker->industry->name }}</p>
                 <p>Registered: {{ $seeker->user->created_at }}</p>
-                <p>Last Profile Update: {{ $seeker->user->updated_at }}</p>
+                <p>Last CV Update: {{ $seeker->user->updated_at }}</p>
             </div>
             <div class="col-md-4 col-12 text-md-right text-left">
                 <p><a href="mailto:{{ $seeker->user->email }}" class="orange">{{ $seeker->user->email }}</a></p>
