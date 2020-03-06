e2f08df67059b404c8ceacea55cb414c1f9d71ba
Obare C. Brian <brian@emploi.co>
2020-03-06 11:45:38 +0300

    meta for job seekers registration page

--- resources/views/seekers/register/index.blade.php
+++ resources/views/seekers/register/index.blade.php
@@ -9,6 +9,16 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
 @section('content')
 @section('user_title','Job Seeker Registration')
 
+@section('meta-include')
+<meta property="og:image" content="/images/500g.png">
+<meta property="og:image:width"   content="500" />
+<meta property="og:image:height"  content="500" />
+
+<meta property="og:url"           content="{{ url('/job-seekers') }}/" />
+<meta property="og:title"         content="Let's wake up your dream" />
+<meta property="og:description"   content="Create a free job seeker account and grow your career." />
+@endsection
+
 
 <form method="POST" action="/create-account">
     @csrf
