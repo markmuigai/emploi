a7aedc899c302480bca187e5d9fc581eb7112972
Obare C. Brian <brian@emploi.co>
2020-03-16 12:44:00 +0300

    update corona message

--- resources/views/components/exit-popup.blade.php
+++ resources/views/components/exit-popup.blade.php
@@ -101,12 +101,10 @@
                     <div class="card">
                         <div class="card-body">
                             <p class="card-text">
-                            	<strong>COVID-19</strong><br>
-								COVID-19 is the disease caused by the new coronavirus that emerged in China in December 2019.<br>
+                            	<strong>COVID-19</strong> is the disease caused by the new coronavirus that emerged in China in December 2019.<br><br>
 
-								COVID-19 symptoms include <a href="https://www.health.harvard.edu/diseases-and-conditions/coronavirus-resource-center" target="_blank" style="color: red; font-weight: bold;">cough, fever and shortness of breath</a>. COVID-19 can be severe, and some cases have caused death.<br>
-								The new coronavirus can be spread from person to person. It is diagnosed with a laboratory test.<br>
-								There is no coronavirus vaccine yet. Prevention involves <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/myth-busters" target="_blank" style="color: green; text-decoration: underline;">frequent hand-washing, coughing into the bend of your elbow and staying home </a>when you are sick.</p>
+								It's symptoms include <a href="https://www.health.harvard.edu/diseases-and-conditions/coronavirus-resource-center" target="_blank" style="color: green; text-decoration: underline;">cough, fever and shortness of breath</a>. COVID-19 can be severe, and some cases have caused death.<br>
+								Prevention of spread involves <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/myth-busters" target="_blank" style="color: green; text-decoration: underline;">frequent hand-washing, coughing into the bend of your elbow and staying home </a>when you are sick.</p>
                         </div>
                         <div class="card-footer">
                             <a href="https://www.worldometers.info/coronavirus/" target="_blank" class="btn btn-orange">View Outbreak</a>
