08d8f519885e0b14ac8cf827ade60973f645ac8a
kirarit <davidkirarit@gmail.com>
2020-03-18 09:11:34 +0300

    navbar error

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,50 +5,50 @@
         </button>
         <a class="navbar-brand" href="/"><img src="{{ asset('images/logo-alt.png') }}" alt="Emploi Logo" /></a>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto mr-0">
                 <!-- EMPLOYER SIDEBAR -->
                 @if( isset(Auth::user()->id) && Auth::user()->role == 'employer' )
                 <li class="nav-item d-md-none d-block">
-                    <a class="nav-link" href="/employers/dashboard">{{ __('other.dashboard') }}</a>
+                    <a class="nav-link" href="/employers/dashboard">Dashboard</a>
                 </li>
                 <li class="nav-item d-md-none d-block">
-                    <a class="nav-link" href="/profile">{{ __('other.profile') }}</a>
+                    <a class="nav-link" href="/profile">Profile</a>
                 </li>
                 <li class="nav-item d-md-none d-block">
-                    <a class="nav-link" href="/employers/jobs">{{ __('other.jobs') }}</a>
+                    <a class="nav-link" href="/employers/jobs">Jobs</a>
                 </li>
                 <li class=" nav-item d-md-none d-block">
-                    <a class="nav-link" href="/employers/browse">{{ __('other.browse_can') }}</a>
+                    <a class="nav-link" href="/employers/browse">Browse Candidates</a>
                 </li>
                 <li class=" nav-item d-md-none d-block">
-                    <a class="nav-link" href="/employers/saved">{{ __('other.saved_ps') }}</a>
+                    <a class="nav-link" href="/employers/saved">Saved Profiles</a>
                 </li>
                 <li class=" nav-item d-md-none d-block">
-                    <a class="nav-link" href="/employers/cv-requests">{{ __('other.r_profiles') }}</a>
+                    <a class="nav-link" href="/employers/cv-requests">Requested Profiles</a>
                 </li>
                 <!-- EMPLOYER SIDEBAR -->
 
                 <!-- JOB SEEKER SIDEBAR -->
                 @elseif( isset(Auth::user()->id) && Auth::user()->role == 'seeker' )
                 <li class="nav-item d-md-none d-block">
-                    <a class="nav-link" href="/job-seekers/dashboard">{{ __('other.dashboard') }}</a>
+                    <a class="nav-link" href="/job-seekers/dashboard">Dashboard</a>
                 </li>
                 <li class="nav-item d-md-none d-block">
-                    <a class="nav-link" href="/profile">{{ __('other.profile') }}</a>
+                    <a class="nav-link" href="/profile">Profile</a>
                 </li>
                 <li class="nav-item d-md-none d-block">
-                    <a class="nav-link" href="/profile/applications">{{ __('other.applications') }}</a>
+                    <a class="nav-link" href="/profile/applications">Applications</a>
                 </li>
                 <li class="nav-item d-md-none d-block">
-                    <a class="nav-link" href="/profile/referees">{{ __('other.referees') }}</a>
+                    <a class="nav-link" href="/profile/referees">Referees</a>
                 </li>
                 <!-- END OF JOB SEEKER SIDEBAR -->
 
                 <!-- ADMIN SIDEBAR -->
                 @elseif( isset(Auth::user()->id) && Auth::user()->role == 'admin' )
                 <li class="nav-item d-md-none d-block">
-                    <a class="nav-link" href="/admin/panel">{{ __('other.dashboard') }}</a>
+                    <a class="nav-link" href="/admin/panel">Dashboard</a>
                 </li>
                 <li class="nav-item d-md-none d-block">
                     <a class="nav-link" href="/admin/posts">Job Posts</a>
@@ -67,22 +67,22 @@
                 </li>
                 @else
                 @endif
-                <li class="nav-item">
-                    <a class="nav-link" href="/vacancies">{{ __('jobs.vacancies') }}</a>
+               <li class="nav-item">
+                    <a class="nav-link" href="/vacancies">Vacancies</a>
                 </li>
                 <li class="nav-item">
                     @guest
-                    <a class="nav-link" href="/companies?hiring=true">{{ __('jobs.whos_hiring') }}</a>
+                    <a class="nav-link" href="/companies?hiring=true">See Who's Hiring</a>
                     @else
 
                     @if(Auth::user()->role == 'employer')
-                    <a class="nav-link" href="/employers/browse">{{ __('other.candidates') }}</a>
+                    <a class="nav-link" href="/employers/browse">Candidates</a>
                     @else
-                    <a class="nav-link" href="/companies?hiring=true">{{ __('jobs.whos_hiring') }}</a>
+                    <a class="nav-link" href="/companies?hiring=true">See Who's Hiring</a>
                     @endif
 
                     @endguest
-
+                    
 
                 </li>
                 <li class="nav-item">
@@ -98,13 +98,13 @@
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <!-- <a class="dropdown-item" href="/employers/dashboard">Dashboard</a> -->
-                        <a class="dropdown-item" href="/employers/browse">{{ __('jobs.browse_cvs') }}</a>
-                        <a class="dropdown-item" href="/employers/publish">{{ __('jobs.advert_jobs') }}</a>
-                        <a class="dropdown-item" href="/employers/premium-recruitment">{{ __('jobs.p_recruit') }}</a>
-                        <a class="dropdown-item" href="/employers/services">{{ __('other.a_services') }}</a>
+                        <a class="dropdown-item" href="/employers/browse">Browse CVs</a>
+                        <a class="dropdown-item" href="/employers/publish">Advertise Jobs</a>
+                        <a class="dropdown-item" href="/employers/premium-recruitment">Premium Recruitment</a>
+                        <a class="dropdown-item" href="/employers/services">All Services</a>
                         @guest
                         @else
-                        <a class="dropdown-item" href="/companies/create">{{ __('other.add_comp') }}</a>
+                        <a class="dropdown-item" href="/companies/create">Add A Company</a>
                         @endguest
                         
                         
@@ -115,15 +115,15 @@
                 @else
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{ __('other.job_seekers') }}
+                        Job Seekers
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="/job-seekers/services">{{ __('other.a_services') }}</a>
-                        <a class="dropdown-item" href="/register">{{ __('jobs.u_cv') }}</a>
-                        <a class="dropdown-item" href="/job-seekers/cv-editing">{{ __('jobs.cv_edit') }}</a>
-                        <a class="dropdown-item" href="/job-seekers/cv-templates">{{ __('jobs.cv_temp') }}</a>
-                        <a class="dropdown-item" href="/job-seekers/premium-placement">{{ __('jobs.p_place') }}</a>
-                        <a class="dropdown-item" href="/job-seekers/faqs">{{ __('other.faqs') }}</a>
+                        <a class="dropdown-item" href="/job-seekers/services">All Services</a>
+                        <a class="dropdown-item" href="/register">Upload CV</a>
+                        <a class="dropdown-item" href="/job-seekers/cv-editing">CV Editing</a>
+                        <a class="dropdown-item" href="/job-seekers/cv-templates">CV Templates</a>
+                        <a class="dropdown-item" href="/job-seekers/premium-placement">Premium Placement</a>
+                        <a class="dropdown-item" href="/job-seekers/faqs">FAQs</a>
                     </div>
                 </li>
                 @endif
@@ -159,25 +159,37 @@
                 </li>
 
                 @if(isset(Auth::user()->id))
+                <li class="nav-item">
+                    <a href="/login" class="btn btn-white px-3">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/join" class="btn btn-orange px-3">Register</a>
+                </li>
+                @endif
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="modal" data-target="#searchModal"><i class="fas fa-search"></i></a>
+                </li>
+
+                @if(isset(Auth::user()->id))
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img src="{{ Auth::user()->getPublicAvatarUrl() }}" class="profile-avatar" alt="Profile">
                     </a>
                     <div class="dropdown-menu dropdown-menu-lg-right dropdown-menu-left" aria-labelledby="navbarDropdown">
                         @guest
-                            <a class="dropdown-item" href="/login">{{ __('auth.login') }}</a>
-                            <a class="dropdown-item" href="/join">{{ __('other.create_profile') }}</a>
-                            <a class="dropdown-item" href="/contact">{{ __('other.c_us') }}</a>
+                            <a class="dropdown-item" href="/login">Login</a>
+                            <a class="dropdown-item" href="/join">Create Profile</a>
+                            <a class="dropdown-item" href="/contact">Contact Us</a>
                         @else
                             @if(Auth::user()->canHandleCvEdits())
-                            <a class="dropdown-item" href="/cv-editing">{{ __('other.e_cvs') }} (
+                            <a class="dropdown-item" href="/cv-editing">Edit Cvs (
                                 {{ count(App\CvEditRequest::where('cv_editor_id',Auth::user()->cvEditor->id)->where('submitted_on',null)->get()) }})</a>
                             @endif
                             @if(Auth::user()->role == 'admin')
-                            <a class="dropdown-item" href="/admin/how-to">Admin {{ __('other.faqs') }}</a>
+                            <a class="dropdown-item" href="/admin/how-to">Admin FAQs</a>
                             @endif
                             @if(Auth::user()->canUseBloggingPanel())
-                            <a class="dropdown-item" href="/my-blogs">{{ __('other.b_panel') }}</a>
+                            <a class="dropdown-item" href="/my-blogs">Blogging Panel</a>
                             @endif
 
                             <?php
@@ -186,9 +198,9 @@
                                     print '<a class="dropdown-item" href="/checkout">Checkout (1)</a>';
                             ?>
 
-                            <a class="dropdown-item" href="/home"><strong>{{ __('other.dashboard') }}</strong></a>
-                            <a class="dropdown-item" href="/profile">{{ __('other.profile') }}</a>
-                            <a class="dropdown-item" href="/logout">{{ __('other.logout') }}</a>
+                            <a class="dropdown-item" href="/home"><strong>Dashboard</strong></a>
+                            <a class="dropdown-item" href="/profile">View Profile</a>
+                            <a class="dropdown-item" href="/logout">Logout</a>
                         @endguest
                     </div>
                 </li>
