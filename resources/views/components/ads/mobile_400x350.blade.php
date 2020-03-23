abb80c357cb746df2c238d8181ff1db6cb55cfcd
Brian Obare <brian@emploi.co>
2020-03-23 12:50:07 +0300

    mobile ads

---
+++ resources/views/components/ads/mobile_400x350.blade.php
@@ -0,0 +1,8 @@
+<!-- mobile-400x350 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:400px;height:350px"
+     data-ad-client="ca-pub-9948474979900683"
+     data-ad-slot="8869648380"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
