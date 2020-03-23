76583fb1d21de5bded05a4bd80be614ad03e4a89
Brian Obare <brian@emploi.co>
2020-03-23 12:32:57 +0300

    ad changes

--- resources/views/components/ads/flat_728x90.blade.php
+++ resources/views/components/ads/flat_728x90.blade.php
@@ -1,9 +1,11 @@
 
-<!-- flat_728x90 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-9948474979900683"
-     data-ad-slot="2026157992"></ins>
+<div style="text-align: center;">
+	<!-- flat_728x90 -->
+	<ins class="adsbygoogle"
+	     style="display:inline-block;width:728px;height:90px"
+	     data-ad-client="ca-pub-9948474979900683"
+	     data-ad-slot="2026157992"></ins>
+</div>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
