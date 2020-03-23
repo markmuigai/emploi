6d7cb2978891190b923ad9074974cf43e03e53b1
Obare C. Brian <brian@emploi.co>
2020-03-23 11:51:17 +0300

    caching update

--- public/sw.js
+++ public/sw.js
@@ -9,6 +9,12 @@ if (workbox) {
 	  new workbox.strategies.StaleWhileRevalidate({
 	    // Use a custom cache name.
 	      cacheName: 'js-cache',
+	      plugins: [
+		      new workbox.expiration.Plugin({
+		        maxEntries: 200,
+		        maxAgeSeconds: 4 * 24 * 60 * 60,
+		      })
+		    ],
 	  })
 	);
 
@@ -32,20 +38,20 @@ if (workbox) {
 	    plugins: [
 	      new workbox.expiration.Plugin({
 	        // Cache only 20 images.
-	        maxEntries: 20,
+	        maxEntries: 200,
 	        // Cache for a maximum of 30 days.
 	        maxAgeSeconds: 30 * 24 * 60 * 60,
 	      })
 	    ],
 	  })
 	);
-	workbox.routing.registerRoute(
-	  new RegExp('/'),
-	  new workbox.strategies.StaleWhileRevalidate({
-	    // Use a custom cache name.
-	    cacheName: 'url-cache',
-	  })
-	);
+	// workbox.routing.registerRoute(
+	//   new RegExp('/'),
+	//   new workbox.strategies.CacheFirst({
+	//     // Use a custom cache name.
+	//     cacheName: 'url-cache',
+	//   })
+	// );
 
 
 } else {
