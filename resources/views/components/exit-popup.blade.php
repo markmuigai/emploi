834c20b53a4dd26b0e0bae1068a3a3632e7b89b9
kirarit <david@emploi.co>
2020-06-17 10:35:21 +0300

    employer/seeker popups

--- resources/views/components/exit-popup.blade.php
+++ resources/views/components/exit-popup.blade.php
@@ -1,3 +1,31 @@
+<style type="text/css">
+    .blink_text {
+
+    animation:1s blinker linear infinite;
+    -webkit-animation:1s blinker linear infinite;
+    -moz-animation:1s blinker linear infinite;
+
+     color: #500095;
+    }
+
+    @-moz-keyframes blinker {  
+     0% { opacity: 1.0; }
+     50% { opacity: 0.0; }
+     100% { opacity: 1.0; }
+     }
+
+    @-webkit-keyframes blinker {  
+     0% { opacity: 1.0; }
+     50% { opacity: 0.0; }
+     100% { opacity: 1.0; }
+     }
+
+    @keyframes blinker {  
+     0% { opacity: 1.0; }
+     50% { opacity: 0.0; }
+     100% { opacity: 1.0; }
+     }
+</style>
 <div id="seekerRegisterModal" class="modal fade" role="dialog">
 	<div class="modal-dialog">
 		<div class="modal-dialog modal-dialog-centered" role="document">
@@ -49,7 +77,7 @@
 			<div class="modal-content">
 				<div class="modal-header" style="background-color: #500095; color: white">
 					
-					<h4 class="modal-title">
+					<h4 class="modal-title" style="text-align: center">
 						Get the Most out of Emploi
 					</h4>
 			
@@ -74,12 +102,12 @@
 	                        <div class="card-body">
 	                            <h5 class="card-title">Featured Package</h5>
 	                            <p class="card-text">
-	                            	Ensure your profile appears first on employer lists, including applications and in searches, 
+	                            	Ensure your profile appears first on employer lists, including applications and in searches. 
 	                            </p>
 	                        </div>
 	                        <div class="card-footer">
 	                            <small class="text-muted">
-	                                <a href="/checkout?product=featured_seeker" class="btn btn-orange-alt">Get - Ksh 159 p.m.</a>
+	                                <a href="/checkout?product=featured_seeker" class="btn btn-orange">Get - Ksh 159 p.m.</a>
 	                            </small>
 	                        </div>
 	                    </div>
@@ -87,7 +115,142 @@
 	                </div>
 	                <div style="text-align: center;">
 	                	<br>
-	                	<a href="{{ url('/job-seekers/cv-editing') }}" class="btn btn-sm btn-orange-alt">Get Professional CV Editing</a>
+	                	<a href="{{ url('/job-seekers/cv-editing') }}" class="btn btn-sm btn-orange-alt">Get Professional CV Editing</span></a>
+	                </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="seekerServices" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+		    <!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header" style="background-color: #500095; color: white">
+					
+					<h4 class="modal-title" style="text-align: center">
+						Get the Most out of Emploi
+					</h4>
+			
+				</div>
+				 <button type="button" style="background-color: #FF5733" class="close" data-dismiss="modal" aria-label="Close" id="closeSeekerServices">
+	                <i class="fas fa-times" aria-hidden="true"></i>
+	              </button>
+				<div class="modal-body">
+					<div class="card-deck">
+	                    <div class="card">
+	                        <div class="card-body">
+	                            <h5 class="card-title">Basic Package</h5>
+	                            <p class="card-text">Get notifications when an employer shortlists you or when your profile is viewed by a shortlisting recruiter.</p>
+	                        </div>
+	                        <div class="card-footer">
+	                            <a href="/checkout?product=seeker_basic" target="_blank" class="btn btn-orange-alt">Get - Ksh 49 p.m.</a>
+	                        </div>
+	                    </div>
+	                    <div class="card">
+	                        <div class="card-body">
+	                            <h5 class="card-title">Featured Package</h5>
+	                            <p class="card-text">
+	                            	Ensure your profile appears first on employer lists, including applications and searches in addition to notifications. 
+	                            </p>
+	                        </div>
+	                        <div class="card-footer">
+	                            <small class="text-muted">
+	                                <a href="/checkout?product=featured_seeker" target="_blank" class="btn btn-orange-alt">Get - Ksh 159 p.m.</a>
+	                            </small>
+	                        </div>
+	                    </div>
+
+	                </div>
+	                <div style="text-align: center;">
+	                	<br>
+	                	<a href="{{ url('/job-seekers/cv-editing') }}" target="_blank" >Get Professional CV Editing at <span class="blink_text">50% Off Discount</span></a>
+	                </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="employerServices" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+		    <!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header" style="background-color: #fff; color: #000">
+					
+					<h4 class="modal-title" style="text-align: center">
+						Get a <span class="blink_text">20% Off Discount</span> on advertising
+					</h4>
+			
+				</div>
+				 <button type="button" style="background-color: #FF5733" class="close" data-dismiss="modal" aria-label="Close" id="closeEmployerServices">
+	                <i class="fas fa-times" aria-hidden="true"></i>
+	              </button>
+				<div class="modal-body">
+					<div class="card-deck">
+	                    <div class="card">
+	                        <div class="card-body">
+	                            <p class="card-text">Your advertisement will be viewed by tens of thousands through our website and social media platforms.</p>
+	                        </div>
+	                        <div class="footer" style="text-align: center">
+	                            <a href="/employers/rate-card" class="btn btn-orange">Get Now</a>
+	                        </div>
+	                    </div>                
+
+	                </div>
+	          	</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="seekerService" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+		    <!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header" style="background-color: #500095; color: white">
+					
+					<h4 class="modal-title" style="text-align: center">
+						Get the Most out of Emploi
+					</h4>
+			
+				</div>
+				 <button type="button" style="background-color: #FF5733" class="close" data-dismiss="modal" aria-label="Close" id="closeSeekerService">
+	                <i class="fas fa-times" aria-hidden="true"></i>
+	              </button>
+				<div class="modal-body">
+					<div class="card-deck">
+	                    <div class="card">
+	                        <div class="card-body">
+	                            <h5 class="card-title">Basic Package</h5>
+	                            <p class="card-text">Get notifications when an employer shortlists you or when your profile is viewed by a shortlisting recruiter.</p>
+	                        </div>
+	                        <div class="card-footer">
+	                            <a href="/checkout?product=seeker_basic" target="_blank" class="btn btn-orange-alt">Get - Ksh 49 p.m.</a>
+	                        </div>
+	                    </div>
+	                    <div class="card">
+	                        <div class="card-body">
+	                            <h5 class="card-title">Featured Package</h5>
+	                            <p class="card-text">
+	                            	Ensure your profile appears first on employer lists, including applications and searches in addition to notifications. 
+	                            </p>
+	                        </div>
+	                        <div class="card-footer">
+	                            <small class="text-muted">
+	                                <a href="/checkout?product=featured_seeker" target="_blank" class="btn btn-orange-alt">Get - Ksh 159 p.m.</a>
+	                            </small>
+	                        </div>
+	                    </div>
+
+	                </div>
+	                <div style="text-align: center;">
+	                	<br>
+	                	<a href="{{ url('/job-seekers/cv-editing') }}" target="_blank">Get Professional CV Editing at <span class="blink_text">50% Off Discount</span></a>
 	                </div>
 				</div>
 			</div>
@@ -205,14 +368,37 @@
 
 	    }
 
-	    // var coronaVirusUpdate = localStorage.getItem("coronaVirusUpdate");
-	    // if(coronaVirusUpdate == null)
-	    // {
-	    	
-	    // 	$('#coronaVirusUpdate').modal();
-	    // 	localStorage.setItem("coronaVirusUpdate", new Date());
-	    // }
-	    
+			    if(path[0] == 'profile' && role == 'seeker')
+			{
+			    	var seekerServices = localStorage.getItem("seekerServices");
+			         if(seekerServices == null)
+			    {    	
+			    	$('#seekerServices').modal();
+			    	localStorage.setItem("seekerServices", new Date());
+			    }
+			}
+
+		  if(path[0] == 'profile' && role == 'employer')
+		{
+		   var employerServices = localStorage.getItem("employerServices");
+		    if(employerServices == null)
+	       {    	
+	    	$('#employerServices').modal();
+	    	localStorage.setItem("employerServices", new Date());
+	       }
+	    }
+
+
+			    if(path[0] == 'vacancies')
+			{
+			    	var seekerService = localStorage.getItem("seekerService");
+			         if(seekerService == null)
+			    {    	
+			    	$('#seekerService').modal();
+			    	localStorage.setItem("seekerService", new Date());
+			    }
+			}
+
     });
     
 </script>
