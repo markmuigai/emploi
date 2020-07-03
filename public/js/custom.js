ee84476935c73b6cea2d445b32df803541aa4671
kirarit <david@emploi.co>
2020-07-03 16:47:11 +0300

    Minify files

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,1 @@
-//$('.counter').countUp();
-// Truncating Text
-$(function() {
-    $(".truncate-short").succinct({
-        size: 30,
-    });
-    $(".truncate").succinct({
-        size: 100,
-    });
-    $(".truncate-long").succinct({
-        size: 150,
-    })
-})
+$(function(){$(".truncate-short").succinct({size:30}),$(".truncate").succinct({size:100}),$(".truncate-long").succinct({size:150})});
