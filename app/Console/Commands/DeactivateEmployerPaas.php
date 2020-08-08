bc27657d8977a8ca5f4ffa2bc5e6a415f65fb2ad
kirarit <davidkirarit@gmail.com>
2020-08-08 07:05:48 +0300

    employer paas deactivate command

---
+++ app/Console/Commands/DeactivateEmployerPaas.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\EmployerSubscription;
+
+class DeactivateEmployerPaas extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:DeactivateEmployerPaas';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Changes the status of employers whose subscription for PAAS has expired';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+  public function handle()
+    {
+        $ends = EmployerSubscription::where('ending','<', now())->where('status','active')->get();
+
+        for($i=0; $i<count($ends); $i++)
+        {
+            $end = $ends[$i];
+
+            $end->status = 'completed';
+            $end->save();
+        }    
+    }
+}
