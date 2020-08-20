77a892b357e282c443be43ad2b87d23915196fb4
kirarit <davidkirarit@gmail.com>
2020-08-20 15:16:01 +0300

    checkout with eclub learn more btn and info

--- resources/views/employers/task.blade.php
+++ resources/views/employers/task.blade.php
@@ -13,6 +13,7 @@ Emploi is the Leading Platform for Talent Assessment and Matching for SME's in A
 @else
 	Professional Requisition
 @endif
+
 </h3><br>
 <div class="container">
     <div class="single">
@@ -115,20 +116,49 @@ Emploi is the Leading Platform for Talent Assessment and Matching for SME's in A
 								</div>
 							</div>
 
-							<hr class="mb-4">							
+							<!-- <hr class="mb-4">	 -->						
 							   @if(isset(Auth::user()->id) && Auth::user()->role == 'employer' && Auth::user()->employer->isOnPaas())
-							   <a href="/employers/dashboard" class="btn btn-primary" id="complete" type="submit">Complete checkout</a>
+							   <p><a href="/employers/dashboard" class="btn btn-primary" id="complete" type="submit">Complete checkout</a>
+							   To pay a total of <b>Ksh. {{ $task->salary + 0.135 * $task->salary }}</b> at the end of the month.</p>
                                 @else
-                               <button class="btn btn-primary" id="submitButton" type="submit">Continue to checkout</button> Or    
-							<a href="/employers/paas?redirectToUrl={{ url()->current() }}" class="btn btn-orange" id="">Checkout with eClub</a>
+                               <button class="btn btn-primary" id="submitButton" type="submit">Continue to checkout</button>  Or   
+							   <a href="/employers/paas?redirectToUrl={{ url()->current() }}" style="float: right" class="btn btn-orange" >Checkout with eClub</a>
                                 @endif						
 						</form>
-						<hr>
-					</div>			
+						<p><button type="button" class="btn btn-orange-alt" style="float: right" data-toggle="modal" data-target="#eclub">Learn more</button></p>
+
+				        <div class="modal fade" id="eclub" role="dialog">
+			                <div class="modal-dialog">
+			              
+			                    <!-- Modal content-->
+			                    <div class="modal-content">
+			                        <div class="modal-header">
+			                           <!--  <button type="button" style="background-color: #FF5733" class="close" data-dismiss="modal">&times;</button> -->
+			                      
+			                        <h4 class="orange">E-CLUB</h4>
+			                        </div>
+			                        <div class="modal-body">
+			                            <h5 class="pt-2">Emploi’s E-club is a membership programme for employers looking to hire part-time professionals.</h5>
+			                            <h6>What You Will Get;</h6> 
+			                            <ul>
+			                            	<li>Friendly and staggered monthly payments. Invoice sent on time.</li>
+			                            	<li>Access to free on-demand HR advisory services and networking opportunities.</li>
+			                            	<li>Access to other Emploi tools for recruitment and talent management.</li>
+			                            	<li>Discounted rates on other Emploi services like job advertisements.</li>
+			                            </ul>                           
+			                     
+			                        </div>
+									<div class="modal-footer">
+									    <button type="button" class="btn btn-orange-alt" data-dismiss="modal">Close</button>
+									</div>      
+			                    </div>
+			                </div>
+						    <hr>
+				     	</div>	
 	      	   
 		    
-		</div>
-		@else
+			</div>
+			@else
 			<p>
 			<a href="/login?redirectToUrl={{ url()->current() }}" class="btn btn-orange">{{ __('auth.login') }}</a>  or  <a href="/register?redirectToUrl={{ url()->current() }}" class="btn btn-orange-alt">Check your Email for Login Credentials</a> to complete this request.
 			</p>
@@ -136,7 +166,7 @@ Emploi is the Leading Platform for Talent Assessment and Matching for SME's in A
 	</div>
 </div>
 
-<script>
+<!-- <script>
 	$().ready(function(){
 		var submit_clicked = false;
 
@@ -152,5 +182,5 @@ Emploi is the Leading Platform for Talent Assessment and Matching for SME's in A
 	    	}
 	    }
 	});
-</script>
+</script> -->
 @endsection
