3833e016136c2dd50aacb3f1656eb64300ff93fb
kirarit <davidkirarit@gmail.com>
2020-08-27 10:12:35 +0300

    leave request management for paas table and model

---
+++ resources/views/admins/paas-applications/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.dashboard-layout')
+
+@section('title','Emploi Admin :: PaaS Task Requests')
+
+@section('description')
+Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs in the job marketplace.
+@endsection
+
+@section('page_title', 'PaaS Task Requests')
+
+@section('content')
+
+<div class="card">
+    <div class="card-body">
+        <div class="row pb-4" style="text-align: right; border-bottom: 0.1em solid #ff5e00">
+            <a href="/admin/panel" class="btn btn-default">
+            <i class="fa fa-arrow-left"></i> Back
+            </a>  
+            <br><hr>
+        </div>
+        <br>
+        <form>
+            <input type="text" placeholder="Search here" name="q" required="" class="form-control">
+        </form>
+        <br>
+        @include('components.ads.responsive')
+        @forelse($tasks as $t)
+        <div class="row">
+            <div class="col-md-8">
+                <h5>{{ $t->title }}</h5>
+                <h4>{{ $t->name }}
+                    <small>{{ $t->created_at->diffForHumans() }}</small>
+                </h4>
+                <p>
+                    Phone: {{ $t->phone_number }}<br>
+                    Email: {{ $t->email }}
+                </p>         
+                
+            </div>
+            <div class="col-md-4 text-right">
+                <p>No. of Applicants: {{ count(\App\PartTimer::Where('task_slug',$t->slug)->get()) }}</p>
+                <p><strong>{{ $t->status }}</strong> </p>
+                <a href="/admin/paas-applications/{{$t->id}}" class="btn btn-orange btn-sm">View</a>
+            </div>
+        </div>
+        <hr>
+        @empty
+        <p class="text-center">
+            No Paas Task Found.
+        </p>
+        @endforelse
+    </div>
+    <div>
+        {{ $tasks->links() }}
+    </div>
+</div>
+
+
+@endsection
