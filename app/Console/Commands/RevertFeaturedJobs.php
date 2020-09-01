6fb520779fd444f7319558ca0dff9be57fde5c9e
kirarit <davidkirarit@gmail.com>
2020-09-01 18:15:04 +0300

    feature veterinarian job

--- app/Console/Commands/RevertFeaturedJobs.php
+++ app/Console/Commands/RevertFeaturedJobs.php
@@ -50,6 +50,7 @@ class RevertFeaturedJobs extends Command
                 ->where('slug', '!=', 'sales-officer-wanted-urgently-apply')
                 ->where('slug', '!=', 'exciting-career-opportunity-in-insurance-sales')
                 ->where('slug', '!=', 'head-of-business-development-at-emploi')
+                ->where('slug', '!=', 'veterinarian-jobs-available-apply-now') 
                 ->get();
 
         
