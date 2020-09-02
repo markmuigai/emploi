e09eeea22aba329fb8ac8f4bed446128486e3579
kelvinmwenda9 <kelvinmwenda9@gmail.com>
2020-09-02 08:45:53 +0300

    verify data send to database when creating a new issue

--- app/Issue.php
+++ app/Issue.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Issue extends Model
 {
     protected $fillable = [
-        'title', 'description','assiognee','start_date','due_date','task_slug','ended_on','status','assigned_on','supervisor','owner','watcher'
+        'title', 'description','assignee','start_on','due_date','task_slug','ended_on','status','assigned_on','supervisor','owner','watcher'
     ];
 }
