ed37a2a662affb7e470131e24991c9d10e8d22e1
kelvinmwenda9 <kelvinmwenda9@gmail.com>
2020-09-02 12:00:18 +0300

    ui changes for issues

--- resources/views/issues/index.blade.php
+++ resources/views/issues/index.blade.php
@@ -10,29 +10,55 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
 
 @section('content')
 
+<style>
+    hr {
+    color:#ddd;
+    background-color: orange; 
+    height:2px;
+    border:none;
+    max-width:100%;
+    }
+</style>
+
 <?php 
     $user = Auth::user();
     $task = App\Task::where('employer_id', $user->employer->id)->get();
 ?>
 
+<div class="row d-flex">
+    <div>
+        <a href="/employers/paas-tasks" class="btn btn-default mr-2 ml-3">
+            <i class="fa fa-arrow-left"></i> Go to Tasks
+        </a>
+    </div>
+
+    <div>
+        <a href="/issues/create" class="btn btn-default">Create New Issue</a> 
+    </div>
+</div>
+
+
+<br>
+
 <div class="card">
-    <div class="card-body">
-        <div class="row" style="text-align: right; border-bottom: 0.1em solid #ff5e00">
-            <a href="/issues/create" class="btn btn-primary">Create Issue</a>  <br><hr>
-        </div>
+    <div class="card-body"> 
         <div class="row">
         @forelse($issues as $i)
 
-
-        
             <div class="col-md-10 offset-md-1">
-                <h4>{{ $i->title }} </h4>
-                <p><strong>Created:</strong> {{ $i->created_at->diffForHumans() }}</p>
-                <p><strong>Updated:</strong> {{ $i->updated_at->diffForHumans() }}</p>
+                <div class="col-md-7">
+                    <h4>{{ $i->title }} </h4>
+                    <p><strong>Created:</strong> {{ $i->created_at->diffForHumans() }}</p>
+                    <p><strong>Updated:</strong> {{ $i->updated_at->diffForHumans() }}</p>
+                </div>
+
+                <div class="col-md-5 pt-4">
                 <p>
-                    <a href="/issues/view/{{ $i->id }}" class="btn btn-orange btn-sm">View</a>
-                    <a href="/issues/edit/{{$i->id}}" class="btn btn-link btn-sm">Edit</a>
+                    <a href="/issues/view/{{ $i->id }}" class="btn btn-orange btn-sm mr-2">View</a>
+                    <a href="/issues/edit/{{$i->id}}" class="btn btn-orange-alt btn-sm ml-2">Edit</a>
                 </p>
+                </div>
+                
                 <hr>
                 
             </div>
@@ -40,7 +66,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
         
         @empty
         <p class="text-center">
-            No Frequently Asked Questions have been found.
+            No Issue found.
         </p>
         </div>
         @endforelse
--- resources/views/issues/show.blade.php
+++ resources/views/issues/show.blade.php
@@ -10,15 +10,26 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
 
 @section('content')
 
+<style>
+    hr {
+    color:#ddd;
+    background-color: orange; 
+    height:2px;
+    border:none;
+    max-width:100%;
+    }
+</style>
+
+<div class="row mr-auto pb-3 ml-1">
+    <a href="/employers/paas-tasks" class="btn btn-default">
+        <i class="fa fa-arrow-left"></i> Go to Tasks
+    </a>
+    
+        <br><hr>
+</div>
+
 <div class="card">
     <div class="card-body">
-        <div class="row" style="text-align: right; ">
-            <a href="/issues" class="btn btn-default">
-                <i class="fa fa-arrow-left"></i> Back
-            </a>
-            
-             <br><hr>
-        </div>
 
         <div class="row">
 
@@ -31,22 +42,24 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                 <p>
                     <?php echo $i->description; ?>
                 </p>
-                <hr>
                 <br>
 
                 <p>
                     <strong>Created:</strong> {{ $i->created_at->diffForHumans() }}<br>
                     <strong>Updated:</strong> {{ $i->updated_at->diffForHumans() }}
 
-                    <a href="/issues/edit/{{ $i->id }}" class="btn btn-link" style="float: right;">
-                        <i class="fa fa-pen"></i> Edit
-                    </a>
-                    <form method="POST" action="/issues/delete/{{ $i->id }}" style="display: inline">
-                        @csrf
-                        {{ method_field('DELETE') }}
-                        <input type="submit" value="Delete" class="btn btn-danger">
-                    </form>
+                    <div class="col-md-12 d-flex pt-4">
+                        <a href="/issues/edit/{{ $i->id }}" class="btn btn-orange-alt mr-5">
+                            <i class="fa fa-pen"></i> Edit
+                        </a>
+                        <form method="POST" action="/issues/delete/{{ $i->id }}">
+                            @csrf
+                            {{ method_field('DELETE') }}
+                            <input type="submit" value="Delete" class="btn btn-orange">
+                        </form>
+                    </div>
                 </p>
+                <hr>
             @endforeach
             </div>
 
