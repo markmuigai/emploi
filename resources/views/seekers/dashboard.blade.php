3f46d9c66ae9ff8541c7465d2a296ff3fb1dcf4d
kirarit <davidkirarit@gmail.com>
2020-09-03 12:19:47 +0300

    jobseeker issues on dashboard, show due date

--- resources/views/seekers/dashboard.blade.php
+++ resources/views/seekers/dashboard.blade.php
@@ -19,7 +19,7 @@ $user = Auth::user();
 <a href="/job-seekers/register-paas" class="btn btn-orange">Apply for part time jobs</a>
 @endif
 @if(isset(Auth::user()->id) && Auth::user()->role == 'seeker' && $user->seeker->isOnPaas())
-<a href="/job-seekers/issues" class="btn btn-orange">View Your PaaS Task Issues</a>
+<a href="/job-seekers/issues" class="btn btn-orange">My Tasks</a>
 @endif
 @if($user->seeker->featured > 0)
 <br><br><h4 align="center">Profile Performance Summary</h4>
--- resources/views/seekers/issue.blade.php
+++ resources/views/seekers/issue.blade.php
@@ -10,23 +10,23 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
 
 @section('content')
 
+<div class="row" style="text-align: right; ">
+    <a href="{{ url()->previous() }}" class="btn btn-orange">
+        <i class="fa fa-arrow-left"></i>Back
+    </a>           
+</div><br>
 <div class="card">
     <div class="card-body">
-        <div class="row" style="text-align: right; ">
-          <a href="{{ url()->previous() }}" class="btn btn-default">
-            <i class="fa fa-arrow-left"></i>Back
-        </a>           
-        <br><hr>
-        </div>
-
         <div class="row">
 
             <div class="col">
 
             @foreach ($issue as $i)
                 <br>
-                <h3><b style="float: right;"> </b>{{ $i->title }}</h3>
-
+                <div class="row d-flex">
+                <h3 class="mr-5 ml-3"><b> </b>{{ $i->title }}</h3>
+                <h5 class="ml-5 orange">Due in {{ Carbon\Carbon::parse($i->due_date)->diffForHumans() }}</h5>
+                </div>
                 <p>
                     <?php echo $i->description; ?>
                 </p>
