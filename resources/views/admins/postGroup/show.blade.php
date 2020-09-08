bc72b2dbac58ead772b8050e1b9fff56cc4e04de
kirarit <davidkirarit@gmail.com>
2020-09-08 15:54:21 +0300

    removed meta on job post group

--- resources/views/admins/postGroup/show.blade.php
+++ resources/views/admins/postGroup/show.blade.php
@@ -6,15 +6,6 @@
 {{ \App\Post::cleanString($postGroup->description)}}
 @endsection
 
-@section('meta-include')
-<!-- <meta property="og:image" content="{{ asset('images/500g.png') }}">
-<meta property="og:image:width"   content="900" />
-<meta property="og:image:height"  content="600" />
-
-<meta property="og:url"           content="{{ url('/vacancies/'.$postGroup->slug) }}/" />
-<meta property="og:title"         content="{{ $postGroup->title }}" />
-<meta property="og:description"   content="{{ $postGroup->description }}" /> -->
-@endsection
 
 @section('content')
 @section('page_title', $postGroup->getTitle() )
