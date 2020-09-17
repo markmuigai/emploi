ed5ba185b55ae0ea36a04d54704171ee6afcaa1b
kirarit <davidkirarit@gmail.com>
2020-09-17 10:08:08 +0300

    remind those on golden to to renew memebership

--- app/Console/Commands/DeactivateSeekerPaas.php
+++ app/Console/Commands/DeactivateSeekerPaas.php
@@ -2,8 +2,12 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
+
 use Illuminate\Console\Command;
 use App\SeekerSubscription;
+
+use App\Jobs\EmailJob;
 
 
 class DeactivateSeekerPaas extends Command
@@ -47,6 +51,43 @@ class DeactivateSeekerPaas extends Command
 
             $end->status = 'completed';
             $end->save();
-        }    
+        }
+
+        $ends = SeekerSubscription::where('ending','>', now())->where('status','active')->get();
+
+        for($i=0; $i<count($ends); $i++)
+        {
+            $end = $ends[$i];
+            $expiry = new Carbon($end->ending);
+            $today = now();
+
+            if($today->diff($expiry)->days == 10){
+
+            $caption = "Renew Golden Club Membership";
+            $contents = "Your Golden Club memebership which enables you to be placed for part time jobs from top employers, will expire in ".$today->diff($expiry)->days." days <br>
+                      You will nolonger be eligible for the following benefits: <br>
+                      <ul>
+                          <li>Guaranteed placement on an on-demand basis to more than one company.</li>
+                          <li>Access to profession-based training and development opportunities.</li>
+                          <li>Increased chances for eventual permanent employment.</li>
+                          <li>Guaranteed income after successful placement.</li>
+                          <li>Great networking opportunities with top employers.</li>
+                      </ul>
+
+                      <br>
+
+                    <a href=".url('/checkout?product=golden_club').">Renew Golden Club</a>
+                     
+                    <br><br>
+
+                    <p>If you require further information regarding this package, kindly <a href='".url('/contact')."'>Contact Us</a>.</p>
+
+                    Thank you for choosing Emploi.
+                    
+
+                    <br>";
+                    EmailJob::dispatch( $end->name, $end->email, 'Renew Golden Club Membership', $caption, $contents);
+            }
+        }     
     }
 }
