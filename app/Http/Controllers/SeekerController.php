dbf12e9650452f4b92574b25f8f43d40d1bddcd0
kirarit <davidkirarit@gmail.com>
2020-09-22 11:25:33 +0300

    golden free for 1 month first time users only

--- app/Http/Controllers/SeekerController.php
+++ app/Http/Controllers/SeekerController.php
@@ -134,6 +134,8 @@ class SeekerController extends Controller
      public function getPaas(Request $request)
     {   
 
+        $user=Auth::user();
+
          $request->validate([
             'firstname'   =>  ['required','max:50','string'],
             'lastname'    =>  ['required','max:50','string'],
@@ -141,105 +143,46 @@ class SeekerController extends Controller
             'phone_number' => ['required', 'string', 'max:15'],
             'industry'    =>  ['integer']
         ]);
-        
-        $user = User::where('email',$request->email)->first();
-        $created = false;
 
-        if(isset($user->id) && $user->userpermission->permission_id == 3){
-           return \Redirect::route('eclub')->with('fail','We noted you are registered as an employer. Kindly join E-Club for employers.');
-        }
-        if(isset($user->id) && $user->userpermission->permission_id == 2){
-            die("This Product is only for Employers and Jobseekers");
-        }
-                if(!isset($user->id))
-        {
-            $username = explode(" ", strtolower($request->name));
-            $username = implode("-", $username).rand(1000,30000);
-            $password = User::generateRandomString(10);
-            $created = true;
-
-            $user = User::create([
-                'name' => $request->firstname. ' ' .$request->lastname,
-                'email' => $request->email,
-                'username' => $username,
-                'email_verification' => User::generateRandomString(10),
-                'password' => Hash::make($password),
-            ]);
-
-            Referral::creditFor($request->email);         
-
-            $seeker = Seeker::create([
-                'user_id' => $user->id,
-                'public_name' => $username,
-                'country_id' => 1,
-                'phone_number' => $request->phone_number,
-                'industry_id' => $request->industry
-            ]);
-
-                $perm = UserPermission::create([
-                'user_id' => $user->id, 
-                'permission_id' => 4
-            ]);        
-                
-
-
-            $caption = "Thank you for registering your profile on Emploi as a Job Seeker.";
-            $contents = "Here are your login credentials for Emploi: <br>
-            username: $username <br>
-            Password: $password <br>
-            <br>
-
-            Verify your account <a href='".url('/verify-account/'.$user->email_verification)."'>here</a> and finish setting up your account for employers to easily find and shortlist you.
-            <br>
-
-            <br>
-                    ";
-            EmailJob::dispatch($user->name, $user->email, 'Verify Emploi Account', $caption, $contents);
-
-            }
-             $js = SeekerSubscription::create([
+         $e = SeekerSubscription::where('email',$request->email)->first();
+         if(isset($e))
+         {
+                $js = SeekerSubscription::create([
                 'user_id' => $user->id,
                 'name' => $request->firstname. ' ' .$request->lastname,
                 'phone_number' => $request->phone_number,
                 'email' => $user->email,
                 'industry_id' => $request->industry
             ]);
+            if(isset($js->id)){
+                if (app()->environment() === 'production'){
+                 Notification::send(SeekerSubscription::first(),new PaasSubscribed('GOLDEN CLUB: '.$js->name.' with contact details  '.$js->email.' and  '.$js->phone_number.'  has submitted subscription for Golden Club Membership.'));
+                }         
+            return redirect('/checkout?product=golden_club');  
+            }         
+         }     
 
-            //     $invoice = Invoice::create([
-            //     'slug' => Invoice::generateUniqueSlug(11),
-            //     'first_name' => $request->firstname,
-            //     'last_name' => $request->lastname,
-            //     'phone_number' => isset($request->phone_number) ? $request->phone_number : null,
-            //     'email' => $user->email,
-            //     'description' => 'Payment for Golden Club Membership',
-            //     'sub_total' => 2750.00
-            // ]);
-            //     if(isset($invoice->id))
-            // {
-            //     $message = 'Invoice '.$invoice->slug.' totalling to  Ksh '.$invoice->sub_total.' created on Emploi. Customer: '.$invoice->first_name.', email: '.$invoice->email;
-            //     $invoice->remind('email');
-            // }
+         else
 
-           
-        if (isset($js->id))
-
+         {
+                $js = SeekerSubscription::create([
+                'user_id' => $user->id,
+                'name' => $request->firstname. ' ' .$request->lastname,
+                'phone_number' => $request->phone_number,
+                'email' => $user->email,
+                'industry_id' => $request->industry
+            ]);
+           if(isset($js->id)){
             $user->seeker->activateFreeGoldenClub();
-        {    
-            if (app()->environment() === 'production')
-             Notification::send(SeekerSubscription::first(),new PaasSubscribed('GOLDEN CLUB SUBSCRIPTION: '.$js->name.' with contact details  '.$js->email.' and  '.$js->phone_number.'  has submitted subscription for Golden Club Membership.'));
-        }
-         
-        if (Auth::guest())
-            {
-             return redirect('/job-seekers/dashboard');
-         }
-         return \Redirect::to("/login?redirectToUrl=/job-seekers/dashboard");
 
-        // if (Auth::guest()) {
-        // return \Redirect::to("/login?redirectToUrl=/checkout?product=golden_club");
-        // }
-        // return redirect('/checkout?product=golden_club');
-    }
+            if (app()->environment() === 'production'){
+             Notification::send(SeekerSubscription::first(),new PaasSubscribed('GOLDEN CLUB SUBSCRIPTION: '.$js->name.' with contact details  '.$js->email.' and  '.$js->phone_number.'  has submitted subscription for Golden Club Membership.'));
+                }
+            }
+              return redirect('/job-seekers/dashboard');
+         }
+    }             
+        
 
     public function leaveContact(Request $request)
     { 
