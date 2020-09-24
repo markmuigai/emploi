671753881705dfcf07daa7b2f8d2947215b8410d
kirarit <davidkirarit@gmail.com>
2020-09-24 11:02:29 +0300

    added responsive ads on companies featured

--- resources/views/companies/featured/featured.blade.php
+++ resources/views/companies/featured/featured.blade.php
@@ -29,18 +29,23 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
 
 <div class="container">
     <div class="col-md-12">
-    	<div class="col-md-8 col-md-offset-2"><br>
-    	<h3 class="orange">Featured Company</h3>
-		    <h5>Featured Companies get more views on our platform.</h5>
+      <div class="row">
+        	<div class="col-md-8"><br>
+        	<h3 class="orange">Featured Company</h3>
+    		    <h5>Featured Companies get more views on our platform.</h5>
 
-		    <ul style="">
-		    	<li>Featured on our main page.</li>
-		    	<li>Featured on one vacancy notification email.</li>
-		    	<li>Vacancies rank top of the list.</li>
-		    </ul>
-		    <a href="/checkout?product=featured_company" class="btn btn-orange">Request</a>
-		    <div class="blink pt-4"><a href="/job-seekers/register-paas" style="text-decoration: none;"><span class="span">Get free one job post for subscribing to this package</span></a></div>		   
-		</div>              
+    		    <ul style="">
+    		    	<li>Featured on our main page.</li>
+    		    	<li>Featured on one vacancy notification email.</li>
+    		    	<li>Vacancies rank top of the list.</li>
+    		    </ul>
+    		    <a href="/checkout?product=featured_company" class="btn btn-orange">Request</a>
+    		    <div class="blink pt-4"><a href="/job-seekers/register-paas" style="text-decoration: none;"><span class="span">Get free one job post for subscribing to this package</span></a></div>		   
+    		</div>
+
+        <div class="col-md-2">
+           @include('components.ads.responsive')
+        </div>              
     </div>
 </div>
 @endsection
