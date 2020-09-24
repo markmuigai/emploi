818a08847b0c82599c3f26158bbdd8a608b3583c
kirarit <davidkirarit@gmail.com>
2020-09-24 10:45:38 +0300

    featured comapnies

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -236,4 +236,9 @@ class CompanyController extends Controller
         return view('companies.featured.featured-failed')
                 ->with('company',$company);
     }
+
+       public function Featured()
+    {
+      return view('companies.featured.featured');
+    }
 }
