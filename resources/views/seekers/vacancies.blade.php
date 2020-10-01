a9e9364aa45195bb999bc35ecf91ae6a2c02cc0b
kirarit <davidkirarit@gmail.com>
2020-10-01 09:07:37 +0300

    fixed broken links on vacancies industries link

--- resources/views/seekers/vacancies.blade.php
+++ resources/views/seekers/vacancies.blade.php
@@ -163,7 +163,7 @@ Emploi is the Leading Platform for Talent Assessment and Matching for SME's in A
                 </p>
                 <p>Current Openings: <strong>{{ $post->positions }} Position{{ $post->positions == 1 ? '' : 's' }}</strong></p>
                 <p>
-                    <a href="/vacancies/{{ $post->industry->name }}" title="View {{ $post->industry->name }} jobs">
+                    <a href="/vacancies/{{ $post->industry->slug }}" title="View {{ $post->industry->name }} jobs">
                         <i class="fa fa-briefcase"></i> {{ $post->industry->name }}
                     </a>
                 </p>
