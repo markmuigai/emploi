9ad8b99ba827307b9e117883ba1fe2c1331c4a62
kirarit <davidkirarit@gmail.com>
2020-10-05 15:16:39 +0300

    label for status on job posts admin panel

--- resources/views/admins/posts/index.blade.php
+++ resources/views/admins/posts/index.blade.php
@@ -44,7 +44,7 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                     </div>
                     <div class="col-md-3 text-md-right text-left">
                         <p>Open for {{ $p->daysSince  }} days</p>
-                        <p>{{ $p->status }}</p>
+                        <p>Status: {{ $p->status }}</p>
                     </div>
                 </div>
                 <hr>
