01b48f39bb36bc101afaa799370a5f9b8bc32a34
Mark Muigai <markmuigai@gmail.com>
2020-10-14 09:49:19 +0300

    Added self assessment controller

---
+++ app/Http/Controllers/JobSeeker/SelfAssessmentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\JobSeeker;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SelfAssessmentController extends Controller
+{
+    /**
+     * Display a listing of the self assessment.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new self assessment.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // Return view
+        return('v2.seekers.self-assessment');
+    }
+
+    /**
+     * Store a newly created self assessment in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified self assessment.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified self assessment.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified self assessment in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified self assessment from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
