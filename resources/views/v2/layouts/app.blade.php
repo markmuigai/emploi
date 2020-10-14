b4b6b2b48013d7e7ddb97a02a61c9fccedc454af
kirarit <davidkirarit@gmail.com>
2020-10-14 08:27:32 +0300

    change title to Welcome to Emploi

--- resources/views/v2/layouts/app.blade.php
+++ resources/views/v2/layouts/app.blade.php
@@ -28,7 +28,7 @@
         <!-- Responsive CSS -->
         <link rel="stylesheet" href="{{asset('assets-v2/css/responsive.css')}}">
 
-        <title>Jecto - Job Board and Portal HTML Template</title>
+        <title>Welcome to Emploi - The Premier Online Job Placement Platform in Africa</title>
 
         <link rel="icon" sizes="512x512" href="{{ asset('images/favicon.png') }}">
     </head>
