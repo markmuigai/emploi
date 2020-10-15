9249615184f05c31e8e8d6f844e821490403fdc0
kirarit <davidkirarit@gmail.com>
2020-10-15 17:32:20 +0300

    align blogs on welcome page

--- resources/views/v2/components/blogs.blade.php
+++ resources/views/v2/components/blogs.blade.php
@@ -2,30 +2,31 @@
     <?php
         $blogs = \App\Blog::where('status','active')->take(3)->get();
     ?>
-    <!-- FEATURED JOBS -->
-       <div class="row">
+    <!-- Blogs -->
+
+    <div class="row">
         @foreach($blogs as $b)
-            <div class="col-sm-6 col-lg-4">
-                <div class="blog-item">
-                    <div class="top">
-                        <a href="/blog/{{ $b->slug }}">
-                            <img src="/storage/blogs/{{ $b->image1 }}" alt="{{ $b->title }}">
-                        </a>
-                    </div>
-                    <span>{{ $b->category->name }}</span>
-                    <h3>
-                        <a href="/blog/{{ $b->slug }}"> {{ $b->title }} </a>
-                    </h3>
-                    <p class="truncate">{!!html_entity_decode($b->Preview)!!}</p>                           
-                    <div class="cmn-link">
-                        <a href="/blog/{{ $b->slug }}">
-                            <i class="flaticon-right-arrow one"></i>
-                      <a href="{{ url('blog/'.$b->slug) }}" class="orange">Read More</a>
-                            <i class="flaticon-right-arrow two"></i>
-                        </a>
-                    </div>
+        <div class="col-sm-6 col-lg-4">
+            <div class="blog-item">
+                <div class="top">
+                    <a href="blog-details.html">
+                        <img src="/storage/blogs/{{ $b->image1 }}" alt="{{ $b->title }}">
+                    </a>
+                </div>
+                <span>{{ $b->category->name }} </span>
+                <h3>
+                    <a href="/blog/{{ $b->slug }}"> {{ $b->title }} </a>
+                </h3>
+                <p>{!!html_entity_decode($b->longPreview(100))!!}</p>
+                <div class="cmn-link">
+                    <a href="blog-details.html">
+                        <i class="flaticon-right-arrow one"></i>
+                        Learn More
+                        <i class="flaticon-right-arrow two"></i>
+                    </a>
                 </div>
             </div>
-            @endforeach
-        </div>
-
+        </div> 
+        @endforeach          
+    </div>
+    <!-- End of Blogs -->
