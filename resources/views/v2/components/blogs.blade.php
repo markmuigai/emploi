24a61389a0bca4e7185c76e960c514266019fbab
Mark Muigai <markmuigai@gmail.com>
2020-10-16 16:04:03 +0300

    Set blog title min height

--- resources/views/v2/components/blogs.blade.php
+++ resources/views/v2/components/blogs.blade.php
@@ -10,16 +10,18 @@
             <div class="blog-item">
                 <div class="top">
                     <a href="blog-details.html">
-                        <img src="/storage/blogs/{{ $b->image1 }}" alt="{{ $b->title }}">
+                        <img src="{{asset('assets-v2/img/home-one/blog1.jpg')}}" alt="{{ $b->title }}">
                     </a>
                 </div>
                 <span>{{ $b->category->name }} </span>
-                <h3>
-                    <a href="/blog/{{ $b->slug }}"> {{ $b->title }} </a>
-                </h3>
-                <p>{!!html_entity_decode($b->longPreview(100))!!}</p>
-                <div class="cmn-link">
-                    <a href="blog-details.html">
+                <div class="blog-content" style="height: 160px;">
+                    <h3>
+                        <a href="/blog/{{ $b->slug }}"> {{ $b->title }} </a>
+                    </h3>
+                    <p>{!!html_entity_decode($b->longPreview(100))!!}</p>
+                </div>
+                <div class="cmn-link d-flex align-self-end">
+                    <a href="blog-details.html" class="align-self-end">
                         <i class="flaticon-right-arrow one"></i>
                         Learn More
                         <i class="flaticon-right-arrow two"></i>
