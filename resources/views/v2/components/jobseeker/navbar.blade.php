5b907cf99835931b2c0558694b2fbc1c688825e3
Mark Muigai <markmuigai@gmail.com>
2020-10-16 13:18:24 +0300

    Added avatar dropdown to navbar v2

--- resources/views/v2/components/jobseeker/navbar.blade.php
+++ resources/views/v2/components/jobseeker/navbar.blade.php
@@ -119,6 +119,24 @@
                                 <a href="/v2/join" style="padding-left" class="btn btn-login btn-orange btn-register btn-outline px-3">{{ __('auth.register') }}</a>
                             </li>
                         @endif
+                        @auth
+                        <li class="nav-item">
+                            <span class="tooltip-span two">New</span>
+                            <img src="{{ Auth::user()->getPublicAvatarUrl() }}" height="30px" class="profile-avatar mp-3" alt="Profile">
+                            <i class='bx bx-chevron-down text-white'></i>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/v2/home"><strong>{{ __('other.dashboard') }}</strong></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/v2/profile">{{ __('other.profile') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/v2/logout">{{ __('auth.logout') }}</a>
+                                </li>
+                            </ul>
+                        </li>
+                        @endauth
                     </ul>
                 </div>
             </nav>
