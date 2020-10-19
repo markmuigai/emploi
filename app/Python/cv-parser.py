aae07324b76d26a17e611146a95de43ea3c0208b
Mark Muigai <markmuigai@gmail.com>
2020-10-19 18:58:36 +0300

    Parser minor update

--- app/Python/cv-parser.py
+++ app/Python/cv-parser.py
@@ -5,9 +5,8 @@ import spacy
 
 # nltk.download('stopwords')
 
-path = f'{pathlib.Path.cwd()}' + '/app/Python//Sample CVs/alice_cv.pdf'
+path = f'{pathlib.Path.cwd()}' + '/app/Python/Sample CVs/alice_cv.pdf'
 data = ResumeParser(path).get_extracted_data()
 
 print(data)
 
-print('hey')
