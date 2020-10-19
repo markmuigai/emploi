8680105a7f84c3c833dc4ff35f42b649f97f3618
Mark Muigai <markmuigai@gmail.com>
2020-10-19 11:13:34 +0300

    Setup careerjet ads

---
+++ resources/views/v2/components/ads/careerjet.blade.php
@@ -0,0 +1,25 @@
+<style>
+    div.cjjobbox{
+      padding: 25px 35px 30px;
+      border: 1px dashed #4f4f4f;
+      border-left: 2px solid #4f4f4f;
+      width: 100%;
+      background-color: #FFFFFF ;
+      position: relative;
+      -webkit-transition: 0.5s all ease;
+      transition: 0.5s all ease;
+      margin-bottom: 30px;
+      /* font-family: Arial, sans-serif ; */
+      /* font-size: 13px ; */
+      /* color: #000000; */
+      text-align: left;
+    }
+    div.cjjobbox a{
+      color: #198ADC ;
+    }
+   </style>
+   <div class="cjjobbox mx-3">
+   <script type="text/javascript" src="https://www.careerjet.co.ke/partners/js_jobbox.html?s=&l=Kenya&n=5&lid=119809&nfr=1&ntt=1">
+   </script>
+   </div>
+   
