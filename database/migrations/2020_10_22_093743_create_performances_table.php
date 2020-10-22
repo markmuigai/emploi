a2ade6db2161ab774251dd52b09d30c697bf8025
kirarit <davidkirarit@gmail.com>
2020-10-22 16:44:24 +0300

    questions, choices and performances\ seeder

---
+++ database/migrations/2020_10_22_093743_create_performances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerformancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->nullable();
+            $table->string('email',100);
+            $table->string('name',100);
+            $table->string('result');
+            $table->string('questions')->nullable();
+            $table->integer('difficulty_level');
+            $table->string('optional_message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('performances');
+    }
+}
