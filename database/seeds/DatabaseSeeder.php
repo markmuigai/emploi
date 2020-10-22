3908dd396c3a225a8c060fda9bb497d5f7bb8051
Mark Muigai <markmuigai@gmail.com>
2020-10-22 17:51:27 +0300

    Imported seeder files in database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,9 +7,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserPermissionSeeder::class,
+            UserSeeder::class,
             RawDataSeeder::class,
-            // JobsSeeder::class,
-            // UserSeeder::class
+            BlogSeeder::class,
+            CompanySeeder::class,
+            EmployerSeeder::class,
+            JobsSeeder::class,
+            FaqSeeder::class,
+            JobApplicationSeeder::class,
+            JobsSeeder::class,
+            PostGroupJobSeeder::class,
+            PostSeeder::class,
+            SeekerSeeder::class,
+            AssessmentSeeder::class
         ]);
     }
 }
