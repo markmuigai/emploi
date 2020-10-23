32b20ded096050a1cc1fcb4d00554e0add9ca767
Mark Muigai <markmuigai@gmail.com>
2020-10-23 18:33:45 +0300

    Difficulty rating seeder basic setup

---
+++ database/seeds/DifficultyRatingSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Industry;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DifficultyRatingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::transaction(function () {
+            
+        });
+    }
+}
