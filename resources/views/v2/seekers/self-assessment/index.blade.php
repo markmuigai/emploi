70690abd67bc6a7eae9e2000336c054589e56c83
Mark Muigai <markmuigai@gmail.com>
2020-10-28 16:19:46 +0300

    Show results basic styling

--- resources/views/v2/seekers/self-assessment/index.blade.php
+++ resources/views/v2/seekers/self-assessment/index.blade.php
@@ -5,11 +5,45 @@
     @include('v2.components.jobseeker.navbar')
     <!-- End Navbar -->
         <!-- Resume -->
-        <div class="person-details-area resume-area ptb-100">
+        <div class="assessment-results ptb-100 px-4">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        Self Assessment Results
+                <div class="row justify-content-center">
+                    <div class="col-md-10">
+                        <div class="container p-4">
+                            <h4 class="text-center">My Self Assessment Results</h4>
+                            <div class="container shadow p-3 mb-5 bg-white rounded px-5">
+                                <div class="row d-flex justify-content-between">
+                                    <div class="col-md-4">
+                                        <h5>Assessment 1</h5>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <h5>Score: {{$score}}/{{$performances->count()}}</h5>
+                                    </div>
+                                </div>
+                                @foreach ($performances as $key => $perf)
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <span><strong>{{($key+1).') '.$perf->question->title}}</strong></span>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-10">
+                                            @foreach ($perf->question->choices as $choice)
+                                            <span class="mr-1">{{$choice->value}}</span>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col-md-4">
+                                            Selected: {{$perf->selectedChoice->value}}
+                                        </div>
+                                        <div class="col-md-4">
+                                            Correct choice: {{$perf->question->correctChoice()->value}}
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
