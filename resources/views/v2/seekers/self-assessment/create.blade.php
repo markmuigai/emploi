5d60dbd89ce48912a349cb6c9189eb919c92c71a
Mark Muigai <markmuigai@gmail.com>
2020-10-28 09:32:01 +0300

    Self Assessment Page redesign

--- resources/views/v2/seekers/self-assessment/create.blade.php
+++ resources/views/v2/seekers/self-assessment/create.blade.php
@@ -1,64 +1,50 @@
-@extends('v2.layouts.self-review')
+@extends('v2.layouts.app')
 @section('title','Emploi :: Self Assessment' )
 @section('content')
     <!-- Navbar -->
-    
-    <h3 class="text-center pt-100">Self Assessment</h3>
+    @include('v2.components.jobseeker.navbar')    
     <!-- End Navbar -->
-        <!-- Resume -->
-        <main>
-            <div id="form_container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        @include('v2.components.jobseeker.navbar')
-                        <div id="wizard_container">
-                            <div id="top-wizard">
-                                <div id="progressbar"></div>
-                            </div>
-                            <!-- /top-wizard -->
-                            <form method="POST" action="{{Route('v2.self-assessment.store', ['email' => request()->email])}}" enctype="multipart/form-data">
-                                @csrf
-                                <input id="website" name="website" type="text" value="">
-                                <!-- Leave for security protection, read docs for details -->
-                                <div id="middle-wizard">
-                                    @foreach($questions as $key => $question)
-                                    <div class="step">                                         
-                                        <div class="row">                                                      
-                                            <div class="col-lg-5">
-                                                <div id="left_form">                                     
-                                                    <h2>Question</h2>
-                                                    <p>{{ $question->title }}</p>
-                                                    <a href="index.html#0" id="more_info" data-toggle="modal" data-target="#more-info"><i class="pe-7s-info"></i></a>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-7 right-form">
-                                                <h3 class="main_question"><strong>{{($key+1)."/".$questions->count()}}</strong>Select the right answer</h3>
-                                                <div class="col-md-12">
-                                                    @forelse($question->choices as $c)
-                                                    <input type="radio" class="assessment-choice" name="choices[{{$question->id}}][]" value={{ $c->id }}> {{ $c->value }}<br>
-                                                    @empty
-                                                    @endforelse
-                                                </div>                                 
-                                            </div>
-                                        </div>                                      
+    <div class="pt-100 pb-70 px-4">
+        <h3 class="text-center my-4">Self Assessment</h3>
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-sm-12">
+                <div class="container shadow p-3 mb-5 bg-white rounded px-5">
+                    <h5 class="py-2">Select the correct answer</h5>
+                    <form method="POST" action="{{Route('v2.self-assessment.store', ['email' => request()->email])}}" enctype="multipart/form-data">
+                        @csrf
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                @foreach($questions as $key => $question)
+                                <div class="caurosel-item">
+                                    <p class="">{{ ($key+1).'. '.$question->title }}</p>
+                                    <div class="ml-3">
+                                        @forelse($question->choices as $c)
+                                            <input type="radio" class="assessment-choice" name="choices[{{$question->id}}][]" value={{ $c->id }}> {{ $c->value }}<br>
+                                        @empty
+                                        @endforelse 
                                     </div>
-                                    @endforeach                              
-                                </div>                              
-                                <!-- /middle-wizard -->
-                                <div id="bottom-wizard">
-                                    <button type="button" id="backward" name="backward" class="backward">Previous</button>
-                                    <button type="button" id="forward" name="forward" class="forward">Next</button>
-                                    <button type="submit" class="submit-custom" id="submitBtn">Submit</button>
-                                </div>
-                                <!-- /bottom-wizard -->
-                            </form>                                            
+                                </div>                                                                 
+                                @endforeach 
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                            </a>
                         </div>
-                        <!-- /Wizard container -->
-                    </div>
-                </div><!-- /Row -->
-            </div><!-- /Form_container -->
-           
-        </main>
+                        <div class="py-3">
+                            <button type="button" id="backward" name="backward" class="btn btn-primary backward">Previous</button>
+                            <button type="button" id="forward" name="forward" class="btn btn-primary forward">Next</button>
+                            <button type="submit" class="btn btn-success submit-custom" id="submitBtn">Submit</button>
+                        </div>
+                    </form>
+                </div>       
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('js')
@@ -97,17 +83,17 @@
                 }
             });
 
-            var submit_clicked = false;
+            // var submit_clicked = false;
 
-            $('#submitBtn').click(function(){
-                    submit_clicked = true;
-                });
+            // $('#submitBtn').click(function(){
+            //         submit_clicked = true;
+            //     });
 
-            window.onbeforeunload = function() {
-                if (submit_clicked === false){
-                return "Are you sure you want to leave? Changes will not be saved!";
-                }
-            }
+            // window.onbeforeunload = function() {
+            //     if (submit_clicked === false){
+            //     return "Are you sure you want to leave? Changes will not be saved!";
+            //     }
+            // }
         });
 </script>
 @endsection
