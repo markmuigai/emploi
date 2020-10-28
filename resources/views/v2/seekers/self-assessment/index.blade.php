263332f990a5f4b875e3faa078f3e62d18a955df
Mark Muigai <markmuigai@gmail.com>
2020-10-28 19:39:46 +0300

    Styling to differentiate correct and wrong asnwers

--- resources/views/v2/seekers/self-assessment/index.blade.php
+++ resources/views/v2/seekers/self-assessment/index.blade.php
@@ -11,10 +11,18 @@
                     <div class="col-md-10">
                         <div class="container p-4">
                             <h4 class="text-center">My Self Assessment Results</h4>
+                            <div class="row justify-content-end">
+                                <div class="col-md-4">
+                                    <a class="text-center float-right cmn-btn mb-4" href="#" role="button">
+                                        View Previous Scores
+                                        <i class='bx bx-filter'></i>
+                                    </a>                                  
+                                </div>
+                            </div>
                             <div class="container shadow p-3 mb-5 bg-white rounded px-5">
                                 <div class="row d-flex justify-content-between">
                                     <div class="col-md-4">
-                                        <h5>Assessment 1</h5>
+                                        <h5>Your Latest Assessement</h5>
                                     </div>
                                     <div class="col-md-4">
                                         <h5>Score: {{$score}}/{{$performances->count()}}</h5>
@@ -23,20 +31,26 @@
                                 @foreach ($performances as $key => $perf)
                                     <div class="row">
                                         <div class="col-md-12">
-                                            <span><strong>{{($key+1).') '.$perf->question->title}}</strong></span>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-10">
-                                            @foreach ($perf->question->choices as $choice)
-                                            <span class="mr-1">{{$choice->value}}</span>
-                                            @endforeach
+                                            <span class="{{$perf->correct == 1 ? 'text-success' : 'text-danger'}}">
+                                                <strong>{{($key+1).') '.$perf->question->title}}
+                                                    @if ($perf->correct == 1)
+                                                    <span style="font-size: 25px">
+                                                        <i class='bx bx-check text-success'></i>
+                                                    </span>
+                                                    @else
+                                                    <span style="font-size: 25px">
+                                                        <i class='bx bx-x text-danger'></i>
+                                                    </span>
+                                                    @endif</strong>
+                                            </span>
                                         </div>
                                     </div>
                                     <div class="row mb-3">
+                                        @if ($perf->correct == 0)
                                         <div class="col-md-4">
                                             Selected: {{$perf->selectedChoice->value}}
                                         </div>
+                                        @endif
                                         <div class="col-md-4">
                                             Correct choice: {{$perf->question->correctChoice()->value}}
                                         </div>
