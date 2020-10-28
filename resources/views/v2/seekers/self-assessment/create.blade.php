1a2f9ee7e913af6fefd5c58537392a3e816e6f68
Mark Muigai <markmuigai@gmail.com>
2020-10-28 13:14:51 +0300

    Added js logic to assessments carousel

--- resources/views/v2/seekers/self-assessment/create.blade.php
+++ resources/views/v2/seekers/self-assessment/create.blade.php
@@ -4,7 +4,7 @@
     <!-- Navbar -->
     @include('v2.components.jobseeker.navbar')    
     <!-- End Navbar -->
-    <div class="pt-100 pb-70 px-4">
+    <div class="assessment-section pt-100 pb-70 px-4">
         <h3 class="text-center my-4">Self Assessment</h3>
         <div class="row justify-content-center">
             <div class="col-md-8 col-sm-12">
@@ -12,10 +12,10 @@
                     <h5 class="py-2">Select the correct answer</h5>
                     <form method="POST" action="{{Route('v2.self-assessment.store', ['email' => request()->email])}}" enctype="multipart/form-data">
                         @csrf
-                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                            <div class="carousel-inner">
+                        <div id="carouselExampleControls" id="assessment-carousel" class="carousel assessment-carousel slide" data-ride="carousel">
+                            <div class="carousel-inner mb-5">
                                 @foreach($questions as $key => $question)
-                                <div class="caurosel-item">
+                                <div class="carousel-item {{$key == 0 ? 'active' : ''}}">
                                     <p class="">{{ ($key+1).'. '.$question->title }}</p>
                                     <div class="ml-3">
                                         @forelse($question->choices as $c)
@@ -26,14 +26,6 @@
                                 </div>                                                                 
                                 @endforeach 
                             </div>
-                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                            </a>
                         </div>
                         <div class="py-3">
                             <button type="button" id="backward" name="backward" class="btn btn-primary backward">Previous</button>
@@ -50,38 +42,54 @@
 @section('js')
     <script>
         $().ready(function(){
-            $('#forward').attr('disabled',true);
+            //carousel
+            $('.assessment-carousel').carousel({
+                interval: false
+            })
 
             $('.assessment-choice').click(function() {
                 $('#forward').css('display', 'inline-block')
             });
 
-            $('.assessment-choice').on('change', function () {
-                $('#forward').prop('disabled', !$(this).val());
-            }).trigger('change');
-
-
             var max = {!! json_encode($questions->count()) !!};
 
             var counter = 1
-            
+
             $("#backward").click(function() {
-                counter--
-                
-                $('#submitBtn').css('display', 'none')
+                $('.assessment-carousel').carousel('prev')
             });
 
             $("#forward").click(function() {
-                if(counter==(max-1)){
-                    console.log(counter)
-                    counter++
-                    $('#submitBtn').css('display', 'inline-block')
-                }else{
-                    $('#forward').css('display', 'none')
-                    console.log(counter)
-                    counter++
-                }
+                $('.assessment-carousel').carousel('next')
             });
+
+            $('.assessment-carousel').on('slid.bs.carousel',function (event) {
+                if(event.direction == 'left'){
+                    console.log('next')
+                    $('#backward').css('display', 'inline-block')
+
+                    if(counter==(max-1)){
+                        console.log(counter)
+                        counter++
+                        $('#submitBtn').css('display', 'inline-block')
+                        $('#forward').css('display', 'none')
+                    }else{
+                        // $('#forward').css('display', 'none')
+                        console.log(counter)
+                        counter++
+                    }
+                }else{
+                    counter--
+                    console.log('prev')
+                    if(counter == 1)
+                        $('#backward').css('display', 'none');
+                        $('#forward').css('display', 'inline-block')
+                    if(counter < (max-1))
+                        console.log('max')
+                        $('#submitBtn').css('display', 'none')
+                    console.log(counter)
+                }    
+            })
 
             // var submit_clicked = false;
 
