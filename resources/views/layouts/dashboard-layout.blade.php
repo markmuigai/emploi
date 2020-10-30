820ac6a87d63f8fad5753ba167a4381455816314
kirarit <davidkirarit@gmail.com>
2020-10-30 09:34:09 +0300

    styling on dashboard layout for buttons on employer dashboard be well aligned on mobile

--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -202,16 +202,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
                         <div class="row">
                             @if(isset(Auth::user()->id) && Auth::user()->role == 'employer' && Auth::user()->employer->isOnPaas())
-                            <a href="/messages" class="btn btn-purple mr-1" dusk="create-new-post"><i class="fas fa-comments"></i> Messages ({{ count(\App\Message::Where('to_id',Auth::user()->id)->Where('seen',0)->get()) }})</a>
+                            <a href="/messages" class="btn btn-purple mr-1 my-1" dusk="create-new-post"><i class="fas fa-comments"></i> Messages ({{ count(\App\Message::Where('to_id',Auth::user()->id)->Where('seen',0)->get()) }})</a>
                             @endif
 
                             @if(isset(Auth::user()->id) && Auth::user()->role == 'seeker' && Auth::user()->seeker->isOnPaas())
-                            <a href="/inbox" class="btn btn-purple mr-1" dusk="create-new-post"><i class="fas fa-comments"></i> Messages ({{ count(\App\Message::Where('to_id',Auth::user()->id)->Where('seen',0)->get())}})</a>
+                            <a href="/inbox" class="btn btn-purple mr-1 my-1" dusk="create-new-post"><i class="fas fa-comments"></i> Messages ({{ count(\App\Message::Where('to_id',Auth::user()->id)->Where('seen',0)->get())}})</a>
                             @endif
 
                             @if( isset(Auth::user()->id) && Auth::user()->role == 'employer' )
-                            <a href="/employers/request-paas" class="btn btn-orange-alt mr-2" >Request PartTimer</a>
-                            <a href="/vacancies/create" class="btn btn-orange" dusk="create-new-post"><i class="fas fa-plus"></i> Post A Job</a>
+                            <a href="/employers/request-paas" class="btn btn-orange-alt mr-2 my-1" >Request PartTimer</a>
+                            <a href="/vacancies/create" class="btn btn-orange my-1" dusk="create-new-post"><i class="fas fa-plus"></i> Post A Job</a>
 
                             @elseif( isset(Auth::user()->id) && Auth::user()->role == 'seeker' )
                             <a href="/vacancies" class="btn btn-orange"><i class="fas fa-plus"></i> Apply For A Job</a>
