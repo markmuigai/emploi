d898a1bb53de8eb9e1d37c832c1c13bd6bd446bc
Mark Muigai <markmuigai@gmail.com>
2020-11-03 19:02:26 +0300

    Setup save job UI

--- resources/views/v2/components/jobseeker/vacancy-card.blade.php
+++ resources/views/v2/components/jobseeker/vacancy-card.blade.php
@@ -1,4 +1,9 @@
-<div class="col-sm-6 col-lg-4 mix {{$recommendedJobs->has($post->id) ? 'recommended' : ''}}">
+@if (auth()->user() == null)
+    <div class="col-sm-6 col-lg-4 mix {{$recommendedJobs->has($post->id) ? 'recommended' : ''}}">
+@else
+    <div class="col-sm-6 col-lg-4 mix {{$post->savedByUser(auth()->user()->id) ? 'saved' : ''}}
+{{$recommendedJobs->has($post->id) ? 'recommended' : ''}}">
+@endif
     <div class="cat-item">
         <span id="vacancies-image">
             <a href="/vacancies/{{ $post->slug }}">
@@ -16,8 +21,21 @@
                 </span>
                 @endif
             </div>
-            <div class="col-md-4">
-                <i class='bx bx-heart'></i> Save
+            <div class="col-md-5">
+                <form id="save-post-{{$post->id}}" action="{{ route('v2.favourite.store',['postId' => $post->id]) }}" method="post">
+                    @csrf
+                    @if (auth()->user() && $post->savedByUser(auth()->user()->id) == 1)
+                        <span class="save-icon text-success" data-id="{{$post->id}}">
+                            <i class='bx bxs-heart'></i>
+                            Unsave
+                        </span>
+                    @else
+                        <span class="save-icon" data-id="{{$post->id}}">
+                            <i class='bx bx-heart'></i>
+                            Save
+                        </span>    
+                    @endif
+                </form>
             </div>
         </div>
         <span>{{ $post->positions }} Position{{ $post->positions == 1 ? '' : 's' }} | 
@@ -28,3 +46,18 @@
         </a>
     </div>
 </div>
+
+@section('js') 
+    <script>
+        $( document ).ready(function(e) {
+            console.log( "ready!" );
+            
+            $(".save-icon").click(function() {
+                postID = $(this).attr("data-id");
+
+                console.log(postID)
+                $('#save-post-'+postID).submit()
+            });
+        });
+    </script>
+@endsection  
