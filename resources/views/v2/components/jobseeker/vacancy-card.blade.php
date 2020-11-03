3bdc0e01835a4dad8734752e2706f039edcd07e4
Mark Muigai <markmuigai@gmail.com>
2020-11-03 08:48:46 +0300

    Setup vacancy card component

---
+++ resources/views/v2/components/jobseeker/vacancy-card.blade.php
@@ -0,0 +1,30 @@
+<div class="col-sm-6 col-lg-4 mix o">
+    <div class="cat-item">
+        <span id="vacancies-image">
+            <a href="/vacancies/{{ $post->slug }}">
+                <img src="{{ asset($post->imageUrl) }}" alt="{{ $post->title }}">
+            </a>
+        </span>
+        <h3>
+            <a href="/vacancies/{{$post->slug}}/">{{  $post->getTitle() }}</a>
+        </h3>
+        <div class="row">
+            <div class="col-md-6">
+                @if($post->featured == 'true')
+                <span class="badge badge-pill badge-success mx-1">
+                    <i class="bx bx-star"> </i>Featured
+                </span>
+                @endif
+            </div>
+            <div class="col-md-4">
+                <i class='bx bx-heart'></i> Save
+            </div>
+        </div>
+        <span>{{ $post->positions }} Position{{ $post->positions == 1 ? '' : 's' }} | 
+            Posted {{ $post->since }}
+        </span>
+        <a class="link" href="index-2.html#">
+            <i class="flaticon-right-arrow"></i>
+        </a>
+    </div>
+</div>
