5b3fa0cded4cd750c3f4c2a19b240b406996ffa7
Mark Muigai <markmuigai@gmail.com>
2020-11-03 12:39:40 +0300

    Replaced experience with location in recommender

--- resources/views/v2/components/modals/recommender-parameters.blade.php
+++ resources/views/v2/components/modals/recommender-parameters.blade.php
@@ -5,63 +5,47 @@
                 Select your details
             </h4>
                 <div class="modal-body">
-                <form method="" action="{{route('v2.recommended-parameters')}}">
-                @csrf
-                <div class="job-filter-area pt-2">
-                    <div class="container">
-                        <form>
-                            <div class="row">
-                                <div class="col-sm-12 col-lg-12">
-                                    <div class="form-group">
-                                        <select name="industry">
-                                            <option>Select Your Industry</option>
-                                            @foreach(\App\Industry::active() as $i)
-                                                <option value="{{ $i->id }}">{{ $i->name }}
+                <form method="POST" action="{{route('v2.recommendation.store')}}">
+                    @csrf
+                    <div class="job-filter-area pt-2">
+                        <div class="container">
+                            <form>
+                                <div class="row">
+                                    <div class="col-sm-12 col-lg-12">
+                                        <div class="form-group">
+                                            <select name="industry">
+                                                <option>Select Your Industry</option>
+                                                @foreach(\App\Industry::active() as $i)
+                                                    <option value="{{ $i->id }}">{{ $i->name }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12 col-lg-12">
+                                        <div class="form-group">
+                                            <select class="selectpicker" data-live-search="true" name="location">
+                                                <option>All Locations</option>
+                                                @forelse($locations as $l)
+                                                <option value="{{ $l->id }}" {{ isset($search_location) && $search_location == $l->id ? 'selected=""' : '' }}>
+                                                    {{ $l->name.', '.$l->country->code }}
                                                 </option>
-                                            @endforeach
-                                        </select>
+                                                @empty
+                                                @endforelse
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-lg-12">
+                                        <button type="submit" class="btn cmn-btn">
+                                            Fetch Recommended Jobs
+                                            <i class='bx bx-search-alt'></i>
+                                        </button>
                                     </div>
                                 </div>
-                                <div class="col-sm-12 col-lg-12">
-                                    <div class="form-group">
-                                        <select name="experience">
-                                            <option>Your Experience Level</option>
-                                            <option value="0">No Experience Required</option>
-                                            <option value="6">6 month Experience</option>
-                                            <option value="12">1 year Experience</option>
-                                            <option value="24">2 years Experience</option>
-                                            <option value="36">3 years Experience</option>
-                                            <option value="48">4 years Experience</option>
-                                            <option value="60">5 years Experience</option>
-                                            <option value="72">6 years Experience</option>
-                                            <option value="84">7 years Experience</option>
-                                            <option value="96">8 years Experience</option>
-                                            <option value="108">9 years Experience</option>
-                                            <option value="120">10 years Experience</option>
-                                            <option value="132">11 years Experience</option>
-                                            <option value="144">12 years Experience</option>
-                                            <option value="156">13 years Experience</option>
-                                            <option value="168">14 years Experience</option>
-                                            <option value="180">15 years Experience</option>
-                                            <option value="192">16 years Experience</option>
-                                            <option value="204">17 years Experience</option>
-                                            <option value="216">18 years Experience</option>
-                                            <option value="228">19 years Experience</option>
-                                            <option value="240">20 years Experience</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6 col-lg-12">
-                                    <button type="submit" class="btn cmn-btn">
-                                        Proceed to Assessment
-                                        <i class='bx bx-search-alt'></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
             </div>
         </div>
     </div>
