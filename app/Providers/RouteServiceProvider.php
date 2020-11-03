a04e4d88c10a7997746b6798baa227640cd05554
kirarit <davidkirarit@gmail.com>
2020-11-03 16:18:13 +0300

    map admin routes on route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -111,6 +111,22 @@ class RouteServiceProvider extends ServiceProvider
              ->group(base_path('routes/jobseeker.php'));
     }
 
+        /**
+     * Define admin routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapAdminRoutes()
+    {
+        Route::prefix('v2')
+             ->middleware('admin')
+             ->name('v2.')
+             ->namespace('App\Http\Controllers\Admin')
+             ->group(base_path('routes/admin.php'));
+    }
+
     /**
      * Define the "api" routes for the application.
      *
