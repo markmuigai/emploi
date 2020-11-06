ba2c52695d500eecb4f7953e8cd18e1176ccf28c
kirarit <davidkirarit@gmail.com>
2020-11-06 15:16:22 +0300

    dummy referee data for testing

---
+++ database/seeds/RefereeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RefereeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+      {
+        // run a set of operations within a database transaction
+        DB::transaction(function () {
+            // Get sql file path
+            $sql = database_path('/sql/referees.sql');
+            // collect contents and pass to DB::unprepared
+            DB::unprepared(file_get_contents($sql));
+        });
+    }
+}
