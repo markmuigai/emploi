1fb8c2f77343ffd39d09283e22a347bc1af74b0d
kirarit <davidkirarit@gmail.com>
2020-11-10 09:23:34 +0300

    schedule vacancy emails on saturday

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -50,7 +50,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('EnableProducts')->twiceDaily(7, 17)->emailOutputOnFailure('david@emploi.co')->environments(['production']);
         $schedule->command('DisableProducts')->twiceDaily(6, 16)->emailOutputOnFailure('david@emploi.co')->environments(['production']);
         // $schedule->command('command:SendVacancyEmails')->cron('0 0 * * 2,6')->emailOutputOnFailure('info@emploi.co');
-        $schedule->command('command:SendVacancyEmails')->weeklyOn(4, '01:00')->emailOutputOnFailure('david@emploi.co');
+        $schedule->command('command:SendVacancyEmails')->weeklyOn(6, '01:00')->emailOutputOnFailure('david@emploi.co');
         // $schedule->command('command:RevertFeaturedJobs')->dailyAt('20:00');
         $schedule->command('command:DeactivateExpiredJobs')->dailyAt('08:00');
         $schedule->command('command:ProInvoiceReminder')->monthlyOn(3, '06:30');
