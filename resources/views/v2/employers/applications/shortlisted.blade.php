3e0ea89c6f5e372f776c09739c3c9f5860962564
Mark Muigai <markmuigai@gmail.com>
2020-11-30 17:56:14 +0300

    Setup shortlisted v2

--- resources/views/v2/employers/applications/shortlisted.blade.php
+++ resources/views/v2/employers/applications/shortlisted.blade.php
@@ -1,103 +1,105 @@
-@extends('layouts.dashboard-layout')
+@extends('v2.layouts.app')
 
 @section('title','Emploi :: '.$post->getTitle())
 
-@section('description')
-Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs in the job marketplace.
-@endsection
-
 @section('content')
-@section('page_title', $post->title.' Shortlist')
-
-<?php
-$last_rsi = [];
-?>
-
-<!-- NAV-TABS -->
-<ul class="nav nav-tabs" id="allCandidates" role="tablist">
-    <li class="nav-item">
-        <a class="nav-link " id="#" href="/employers/applications/{{ $post->slug }}" >All ({{ count($post->applications) }})</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active" id="all-candidates-tab"  data-toggle="tab" href="#all-candidates" role="tab" aria-controls="all-candidates" aria-selected="true">Shortlisted ({{ count($post->shortlisted) }})</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" id="selected-tab" href="/employers/applications/{{ $post->slug }}/selected">Selected ({{ count($post->selected) }})</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" id="unrejected-jobs-tab" href="/employers/applications/{{ $post->slug }}/unrejected">Not Rejected ({{ count($post->unrejected) }})</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" id="rejected-jobs-tab" href="/employers/applications/{{ $post->slug }}/rejected">Rejected ({{ count($post->rejected) }})</a>
-    </li>
+@section('page_title', $post->title.' Candidates')
+    <?php
+    $last_rsi = [];
+    ?>
     
-</ul>
-<div class="row">
-    <div class="col-lg-9 col-12">
-        <!-- NAV-TAB CONTENT -->
-        <div class="tab-content pt-2" id="allCandidatesContent">
-            <!-- ALL CANDIDATES -->
-            <div class="tab-pane fade show active" id="all-candidates" role="tabpanel" aria-labelledby="all-candidates-tab">
-                <?php  $kk=0; ?>
+    <!-- Navbar -->
+    @include('v2.components.jobseeker.navbar')
+    <!-- End Navbar -->
 
-                <!-- JOB CARD -->
-                
-                @forelse($pool as $a)
-                    @include('components.applicant')
-                    <?php $kk++; ?>
-                    @if($kk%3==0)
-                        @include('components.ads.responsive')
-                    @endif
-                @empty
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            <p class="text-center">
-                                No applications have been found
-                            </p>
+    <style>
+        .banner{
+            background: purple;
+            height: 10vh;
+            display: flex;
+            align-items: center
+        }
+
+        .banner a{
+            text-decoration: none;
+            transition: 2s;
+        }
+
+        .banner h5{
+            color: #fff;
+            font-weight: 900;
+            text-align: center;
+            margin: 0;
+        }
+    </style>
+
+    <!-- Jobs -->
+    <div class="job-area-list dashboard-area mt-1 ptb-100">
+        <div class="container-fluid px-2">
+            <div class="row">
+                <div class="col-lg-2 pt-5">
+                    @include('v2.components.sidebar.employer')               
+                </div>           
+                 <div class="col-lg-10 jobs-form">
+                    <!-- Jobs -->
+                    <div class="categories-area pt-2 pb-70">
+                        <div class="container-fluid"> 
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="row my-4">
+                                        <div class="col-md-12">
+                                            <div class="sorting-menu mt-3 float-left">
+                                                <ul> 
+                                                    <li class="filter" data-filter="all">1.All Applications</li>
+                                                    <li class="filter" data-filter=".recommended">2.Shortlisted</li>   
+                                                    <li class="filter" data-filter=".saved">3.Manage Interviews</li>
+                                                    <li class="filter" data-filter=".internships">4.Manage Referees</li>
+                                                    <li class="filter" data-filter=".internships">5.Select Candidates</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                    <div class="tab-content" id="v-pills-tabContent">
+                                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                                            <div id="container" class="row">   
+                                                <?php  $kk=0; $shown = []; ?>   
+                                                @forelse($post->featuredApplications as $a)
+                                                    @include('v2.components.employer.candidate-card')
+                                                    <?php $shown[] = $a->id; ?>
+                                                @empty
+                                                @endforelse
+                                                                                
+                                                @forelse($pool as $a)
+                                                    @if(array_search($a->id, $shown) === false)
+                                                        @include('v2.components.employer.candidate-card')
+                                                    @endif
+                                                @empty
+                                                    <div class="card mb-4">
+                                                        <div class="card-body">
+                                                            <p class="text-center">
+                                                                No applications have been found
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                @endforelse
+                                            </div>
+                                            <div class="row">
+                                                <div class="col">
+                                                    {{ $pool->links() }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                @endforelse
-                    
-            </div>
-            
-        </div>
-    </div>
-    <div class="col-lg-3 col-12">
-        <div class="card mt-2">
-            <div class="card-body text-center">
-                <h4>Actions</h4>
-                <div class="d-flex flex-column justify-content-center align-items-center">
-                    <a href="/employers/applications/{{ $post->slug }}/rsi" class="btn btn-sm btn-danger" title="Job Score Model"><i class="far fa-file-alt"></i> Job Score</a>
-                    <p>
-                        @if(!$post->hasModelSeeker())
-                        <small  title="Role Suitability Index Model">Create an Job Score Model to shortlist candidates.</small>
-                        @else
-                        <small title="Role Suitability Index Model">Edit your Job Score Model.</small>
-                        @endif
-                    </p>
-                    @if($post->status == 'active')
-                    <a href="/employers/applications/{{ $post->slug }}/invite" class="btn btn-success btn-sm"><i class="fas fa-share"></i> Invite</a>
-                    @endif
-                    <a href="/employers/applications/{{ $post->slug }}/close" class="btn btn-info btn-sm"> <i class="fas fa-users"></i> Select</a>
+                    <!-- End Jobs -->
                 </div>
-                @if(count($post->shortlisted) > 0)
-                <br><br>
-                <h4 class="m_1">Shortlisted Candidates</h4>
-                @forelse($post->shortlisted as $a)
-                <p class="py-1">
-                    {{ $a->user->name }} | <span class="pull-right">
-                        <a href="/employers/applications/{{ $post->slug }}/{{ $a->id }}/rsi">
-                            {{ $a->user->seeker->getRsi($post) }}%
-                        </a>
-                    </span>
-                </p>
-                @empty
-                @endforelse
-                @else
-                @endif
             </div>
         </div>
     </div>
-</div>
-
+    <!-- End Jobs -->
 @endsection
+
+
