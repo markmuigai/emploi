874fd7d338bc02bcd575f511fc0c827d945d800b
Mark Muigai <markmuigai@gmail.com>
2020-12-01 14:52:47 +0300

    Setup create ad form blade file

---
+++ resources/views/v2/employers/advertise/create.blade.php
@@ -0,0 +1,15 @@
+@extends('v2.layouts.app')
+
+@section('title','Advertise With Us :: Emploi')
+
+@section('content')
+    <!-- Navbar -->
+    @include('v2.components.jobseeker.navbar')
+    <!-- End Navbar -->
+
+    <div class="advertise-form container-fluid ptb-100">
+        <h2 class="mt-4 text-center">
+            Why advertise with us?
+        </h2>  
+    </div>
+@endsection
