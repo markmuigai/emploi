9ec1b402d831b7dcea1c4b9e6050c88716e699e1
Mark Muigai <markmuigai@gmail.com>
2020-12-01 14:30:54 +0300

    Setup employer advertise controller

---
+++ app/Http/Controllers/Employer/AdvertiseController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Employer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdvertiseController extends Controller
+{
+    /**
+     * Display a listing of the advertisement.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new advertisement.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // Request ad form
+        return view('employer.advertise.create');
+    }
+
+    /**
+     * Store a newly created advertisement in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified advertisement.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified advertisement.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified advertisement in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified advertisement from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
