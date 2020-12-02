a4d38c251c9260d8d6a5cddaffeec3a0b8159bec
Mark Muigai <markmuigai@gmail.com>
2020-12-02 16:50:54 +0300

    Added edit interview schedule blade file

---
+++ resources/views/v2/employers/interview/edit.blade.php
@@ -0,0 +1,78 @@
+@extends('v2.layouts.app')
+
+@section('title','Interview Schedule :: Emploi')
+
+@section('description')
+Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs in the job marketplace.
+@endsection
+
+@section('content')
+    @section('page_title', $application->user->name.' Interview')
+
+    <!-- Navbar -->
+    @include('v2.components.jobseeker.navbar')
+    <!-- End Navbar -->
+
+    <!-- Jobs -->
+    <div class="dashboard-area mt-1 ptb-100">
+        <div class="container-fluid px-2">
+            <div class="row">
+                <div class="col-lg-2 pt-5">
+                    @include('v2.components.sidebar.employer')               
+                </div>           
+                 <div class="col-lg-10">
+                    <div class="container-fluid my-4">
+                        {{-- <a href="{{ url()->previous() }}" class="btn btn-primary rounded-pill">
+                            <i class="fa fa-arrow-left"></i> Back
+                         </a> --}}
+                        <h4>
+                            {{$application->user->name}} Interview for {{strtolower($application->post->title)}} position
+                        </h4>
+                        <div class="row justify-content-center">
+                            <div class="col-md-12 shadow p-4">
+                                <form action="{{route('v2.interviews.update', [$application])}}" method="post">
+                                    @method('PUT')
+                                    @csrf
+                                    <div class="form-row">
+                                        <div class="col-md-6">
+                                            <label for="date">Date and Time</label> 
+                                            <input class="form-control rounded-pill" name="date" type="datetime-local" id="meeting-time"
+                                            name="meeting-time" value="{{$interview->formattedDate()}}"
+                                            min="2020-06-07T00:00" max="2022-06-14T00:00">
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="modeOfInterview">Mode of Interview</label>
+                                            <select id="modeOfInterview" name="modeOfInterview" class="interview-mode rounded-pill">
+                                              <option {{$interview->interview_mode == 'online' ? 'selected' : ''}} value="online">Online</option>
+                                              <option {{$interview->interview_mode == 'physical' ? 'selected' : ''}} value="physical">Physical</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="row mt-4">
+                                        <div class="form-group col-md-12">
+                                            <label for="location">Location</label>
+                                            <input type="text" name="location" class="form-control rounded-pill" placeholder="Location"
+                                            value="{{$interview->location ?? ''}}">
+                                          </div>
+                                    </div>
+                                    <div class="row mt-4">
+                                        <div class="form-group col-md-12">
+                                            <label for="interviewDescription">Description</label>
+                                            <textarea class="form-control" name="description" id="interviewDescription" rows="3">
+                                                {{$interview->description ?? ''}}
+                                            </textarea>
+                                          </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary mt-3">
+                                        Update Schedule 
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Jobs -->
+@endsection
