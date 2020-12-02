a9acedc829a72b30b1a46a4dfc15de5c1a14e993
Mark Muigai <markmuigai@gmail.com>
2020-12-02 16:47:59 +0300

    Fetch interview date using custom format

--- app/Interview.php
+++ app/Interview.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Interview extends Model
@@ -9,4 +10,8 @@ class Interview extends Model
     protected $fillable = [
         'date','description','type', 'interview_mode', 'location', 'job_application_id'
     ];
+
+    public function formattedDate(){
+        return Carbon::parse($this->date)->toDateString().'T'.Carbon::parse($this->date)->toTimeString();
+    }
 }
