6e384a68c2ae0c7f1fd75d89ed164da0cc0d563a
Mark Muigai <markmuigai@gmail.com>
2020-12-02 13:05:21 +0300

    Updated arguments in interview date picker

--- resources/views/v2/employers/interview/create.blade.php
+++ resources/views/v2/employers/interview/create.blade.php
@@ -37,8 +37,8 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                                             <label for="date">Date and Time</label> 
 
                                             <input class="form-control rounded-pill" name="date" type="datetime-local" id="meeting-time"
-                                            name="meeting-time" value="2018-06-12T19:30"
-                                            min="2018-06-07T00:00" max="2018-06-14T00:00">
+                                            name="meeting-time" value="2020-12-01T14:30"
+                                            min="2020-06-07T00:00" max="2022-06-14T00:00">
                                         </div>
                                         <div class="col-md-6">
                                             <label for="modeOfInterview">Mode of Interview</label>
