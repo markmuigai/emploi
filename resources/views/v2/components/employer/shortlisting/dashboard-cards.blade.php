156f20c7172204e662cbccb84c4d931f202977d0
Mark Muigai <markmuigai@gmail.com>
2020-12-04 08:35:04 +0300

    Added dashboard cards component

---
+++ resources/views/v2/components/employer/shortlisting/dashboard-cards.blade.php
@@ -0,0 +1,29 @@
+<h3 class="text-center my-2">Manage applicants in different stages of the hiring process</h3>
+<div class="row">
+    <div class="col-md-6">
+        <div class="card p-3">
+            <h4>Job score criteria</h4>
+            <p>Create a job score model to rank candidates</p>
+            <div class="row">
+                <div class="col-md-6">
+                <button class="btn btn-primary btn-sm rounded-pill">
+                    Update ranking criteria
+                </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="card shadow p-3">
+            <h4>Quick actions</h4>
+            <p>Select candidates from our talent database</p>
+            <div class="row">
+                <div class="col-md-6">
+                <button class="btn btn-primary btn-sm rounded-pill">
+                    Browse More Candidates
+                </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
