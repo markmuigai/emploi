d04b5fe69a52ab26b94c7568abe50fe0e0d7f917
Mark Muigai <markmuigai@gmail.com>
2020-12-04 10:05:59 +0300

    Dashboard cards mobile responsiveness

--- resources/views/v2/components/employer/shortlisting/dashboard-cards.blade.php
+++ resources/views/v2/components/employer/shortlisting/dashboard-cards.blade.php
@@ -1,28 +1,29 @@
 <h3 class="text-center my-2">Manage applicants in different stages of the hiring process</h3>
 <div class="row">
     <div class="col-md-6">
-        <div class="card p-3">
+        <div class="card p-3 mb-2">
             <h4>Job score criteria</h4>
-            <p>Create a job score model to rank candidates</p>
+            <p>By creating an RSI Model, you will be able to rank applicants through different criteria e.g education, experience and skills</p>
             <div class="row">
                 <div class="col-md-6">
                 <button class="btn btn-primary btn-sm rounded-pill">
-                    Update ranking criteria
+                    Generate ranking criteria
                 </button>
                 </div>
             </div>
         </div>
     </div>
     <div class="col-md-6">
-        <div class="card shadow p-3">
+        <div class="card p-3">
             <h4>Quick actions</h4>
-            <p>Select candidates from our talent database</p>
-            <div class="row">
-                <div class="col-md-6">
-                <button class="btn btn-primary btn-sm rounded-pill">
+            <p>Select candidates from our talent database or generate a link to share this vacancy</p>
+            <div class="row px-3">
+                <button class="btn btn-primary mr-2 mt-1 btn-sm rounded-pill">
                     Browse More Candidates
                 </button>
-                </div>
+                <button class="btn btn-success mr-2 mt-1 btn-sm rounded-pill">
+                    Share this vacancy
+                </button>
             </div>
         </div>
     </div>
