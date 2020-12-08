b445234631efdb603d92c78ad72cd6ad9f7dfd7e
Mark Muigai <markmuigai@gmail.com>
2020-12-08 14:40:24 +0300

    Updated assessment results mobile responsiveness

--- resources/views/v2/seekers/self-assessment/index.blade.php
+++ resources/views/v2/seekers/self-assessment/index.blade.php
@@ -7,11 +7,11 @@
     @include('v2.components.jobseeker.navbar')
     <!-- End Navbar -->
         <!-- Resume -->
-        <div class="assessment-results ptb-100 px-4">
+        <div class="assessment-results ptb-100">
             <div class="container">
                 <div class="row justify-content-center">
-                    <div class="col-md-10">
-                        <div class="container p-4">
+                    <div class="col-md-12">
+                        <div class="container-fluid p-4">
                             <h4 class="text-center">My Self Assessment Results</h4><br>
                             @if(App\Performance::recentScore(request()->email) != NULL)
                                                 
@@ -26,65 +26,69 @@
                                 </div>                             
                             @endguest
                             @if(auth::user())
-                                <div class="container shadow p-3 mb-5 bg-white rounded px-5">
-                                    <div class="row d-flex justify-content-between">
-                                        <div class="col-md-6">
-                                            <h5>{{ auth::user()->name }}</h5>
+                            <div class="row justify-content-center">
+                                <div class="col-12 col-md-10">
+                                    <div class="container-fluid shadow p-3 mb-3 bg-white rounded px-5">
+                                        <div class="row d-flex justify-content-between">
+                                            <div class="col-md-6">
+                                                <h5>{{ auth::user()->name }}</h5>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <h5>Score: {{ $score/$performances->count() *100 }}%</h5>
+                                            </div>
                                         </div>
-                                        <div class="col-md-4">
-                                            <h5>Score: {{ $score/$performances->count() *100 }}%</h5>
-                                        </div>
-                                    </div>
-                                    @foreach ($performances as $key => $perf)
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <span class="{{$perf->correct == 1 ? 'text-success' : 'text-danger'}}">
-                                                    <strong>{{($key+1).') '.$perf->question->title}}
-                                                        @if ($perf->correct == 1)
-                                                        <span style="font-size: 25px">
-                                                            <i class='bx bx-check text-success'></i>
-                                                        </span>
+                                        @foreach ($performances as $key => $perf)
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <span class="{{$perf->correct == 1 ? 'text-success' : 'text-danger'}}">
+                                                        <strong>{{($key+1).') '.$perf->question->title}}
+                                                            @if ($perf->correct == 1)
+                                                            <span style="font-size: 25px">
+                                                                <i class='bx bx-check text-success'></i>
+                                                            </span>
+                                                            @else
+                                                            <span style="font-size: 25px">
+                                                                <i class='bx bx-x text-danger'></i>
+                                                            </span>
+                                                            @endif</strong>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        @if (isset($perf->question->image))
+                                            <div class="row mb-3">
+                                                <img src="/storage/assessments/{{$perf->question->id}}" height="auto" width="400px" alt="">
+                                                @if ($perf->correct == 0)
+                                                    <div class="col-md-4">
+                                                        @php
+                                                            $options = collect(['a','b','c','d']);
+                                                        @endphp
+                                                            Selected: {{$options->get($perf->choice_id)}}
+                                                    </div>
+                                                @endif
+                                                <div class="col-md-4">
+                                                    Correct choice: {{$perf->question->image->correct_value}}
+                                                </div>
+                                            </div>
+                                        @else
+                                            <div class="row mb-3">
+                                                @if ($perf->correct == 0)
+                                                    <div class="col-md-4">
+                                                        @if ($perf->choice_id == 0)
+                                                            No answer given
                                                         @else
-                                                        <span style="font-size: 25px">
-                                                            <i class='bx bx-x text-danger'></i>
-                                                        </span>
-                                                        @endif</strong>
-                                                </span>
-                                            </div>
-                                        </div>
-                                    @if (isset($perf->question->image))
-                                        <div class="row mb-3">
-                                            <img src="/storage/assessments/{{$perf->question->id}}" height="auto" width="400px" alt="">
-                                            @if ($perf->correct == 0)
+                                                            Selected: {{$perf->selectedChoice->value}}
+                                                        @endif
+                                                    </div>
+                                                @endif
                                                 <div class="col-md-4">
-                                                    @php
-                                                        $options = collect(['a','b','c','d']);
-                                                    @endphp
-                                                        Selected: {{$options->get($perf->choice_id)}}
+                                                    Correct choice: {{$perf->question->correctChoice()->value}}
                                                 </div>
-                                            @endif
-                                            <div class="col-md-4">
-                                                Correct choice: {{$perf->question->image->correct_value}}
                                             </div>
-                                        </div>
-                                    @else
-                                        <div class="row mb-3">
-                                            @if ($perf->correct == 0)
-                                                <div class="col-md-4">
-                                                    @if ($perf->choice_id == 0)
-                                                        No answer given
-                                                    @else
-                                                        Selected: {{$perf->selectedChoice->value}}
-                                                    @endif
-                                                </div>
-                                            @endif
-                                            <div class="col-md-4">
-                                                Correct choice: {{$perf->question->correctChoice()->value}}
-                                            </div>
-                                        </div>
-                                    @endif
-                                    @endforeach
+                                        @endif
+                                        @endforeach
+                                    </div>
                                 </div>
+                            </div>
                             @endif
                             @else
                             <div class="container shadow p-3 mb-5 bg-white rounded px-5">
