fad1fde50e63e0b1c890b7102b51f0c65d54bac2
Mark Muigai <markmuigai@gmail.com>
2020-12-08 09:07:33 +0300

    Setup share this vacancy modal

--- resources/views/v2/components/employer/shortlisting/dashboard-cards.blade.php
+++ resources/views/v2/components/employer/shortlisting/dashboard-cards.blade.php
@@ -21,10 +21,70 @@
                 <button class="btn btn-primary mr-2 mt-1 btn-sm rounded-pill">
                     Browse More Candidates
                 </button>
-                <button class="btn btn-success mr-2 mt-1 btn-sm rounded-pill">
+                <button class="btn btn-success mr-2 mt-1 btn-sm rounded-pill" data-toggle="modal" data-target="#shareVacancy">
                     Share this vacancy
                 </button>
             </div>
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="shareVacancy" tabindex="-1" role="dialog" aria-labelledby="shareVacancyLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="card">
+            <div class="card-body text-center copyright-area">
+                <h2>
+                    Invitation for {{ $post->title }}
+                </h2>
+                <a href="/employers/applications/{{ $post->slug }}" class="btn btn-orange">Applications ({{ count($post->applications) }})</a>
+        
+                @if(!$post->hasModelSeeker())
+                <a href="/employers/applications/{{ $post->slug }}/rsi" class="btn btn-danger"><i class="fas fa-warning"></i> RSI Model Not Created</a>
+                @else
+                <a href="/employers/applications/{{ $post->slug }}/rsi" class="btn btn-purple"><i class="fas fa-check"></i> RSI Model</a>
+                @endif
+        
+                <hr>
+        
+                @if($post->status == 'active')
+                <p>
+                    Share the link below to invite applications for this position.
+                </p>
+                <h4 class="orange my-3"><a href="{{ url('/vacancies/'.$post->slug.'/apply') }}">
+                        {{ url('/vacancies/'.$post->slug.'/apply') }}
+                    </a></h4>
+        
+                    <div class="copyright-item">
+                        <ul>
+                            <li>
+                                <a href="http://www.facebook.com/sharer.php?u={{ url('/vacancies/'.$post->slug.'/apply') }}" target="_blank">
+                                    <i class='bx bxl-facebook'></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/share?url={{ url('/vacancies/'.$post->slug.'/apply') }}&amp;text={{ urlencode($post->title) }}&amp;hashtags=Emploi{{ $post->location->country->code }}" target="_blank">
+                                    <i class='bx bxl-twitter'></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url={{ url('/vacancies/'.$post->slug) }}" target="_blank">
+                                    <i class='bx bxl-linkedin-square'></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                <p>
+                    All applicants <strong>must register a profile</strong> and update their profile
+                </p>
+                @else
+                <p>
+                    This position has been closed and further applications cannot be received.
+                </p>
+                @endif
+                <button type="button" class="btn btn-secondary rounded-pill" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
