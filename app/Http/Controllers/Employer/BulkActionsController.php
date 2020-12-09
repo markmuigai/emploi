b34c799afe668d9939cfbb792a784704067ebfeb
Mark Muigai <markmuigai@gmail.com>
2020-12-09 18:07:11 +0300

    Setup candidates bulk actions controller for employers

---
+++ app/Http/Controllers/Employer/BulkActionsController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Employer;
+
+use App\Post;
+use App\JobApplication;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BulkActionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $post = Post::findOrFail(request()->post);
+
+        $applications = JobApplication::where('post_id',$post->id)->orderBy('id','DESC')->get();    
+
+        if($post->company->user_id != auth()->user()->id)
+            return abort(403);
+
+        if($post->company->user_id == auth()->user()->id)
+        {
+            return view('v2.employers.applications.bulk-actions',[
+                'post' => $post,
+                'pool' => $applications
+            ]);
+        }
+        return redirect()->back();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
