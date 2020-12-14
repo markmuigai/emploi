b57487b450d0dc5fbd8f6d1579ada06ab89769be
Mark Muigai <markmuigai@gmail.com>
2020-12-14 18:38:46 +0300

    Bulk actions modal initial setup

--- resources/views/v2/components/tables/employer/applicants.blade.php
+++ resources/views/v2/components/tables/employer/applicants.blade.php
@@ -5,7 +5,7 @@
             <div class="card shadow my-3">
                 <div class="card-body">
                     <form class="form-inline">
-                        <select class="my-1 mr-sm-2" name="action">
+                        <select id="actions" class="my-1 mr-sm-2" name="action">
                             <option selected>Select an action</option>
                             <option value="shortlist">Shortlist</option>
                             <option value="downloadCV">Download CV</option>
@@ -91,5 +91,33 @@
             });
         }
     });
+
+    // Show modal if send assessment action is selected
+    $("#actions").change(function(){
+        let action = $("#actions").val();
+        
+        if(action == 'sendAssessment'){
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        }
+    });
+
 </script>
 @endsection
