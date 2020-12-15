0648f163e55a99d3fa0de92bf885aed809e11558
kirarit <davidkirarit@gmail.com>
2020-12-15 15:38:51 +0300

    remove from shortlist UI

--- resources/views/v2/components/employer/shortlisting/applicant-card.blade.php
+++ resources/views/v2/components/employer/shortlisting/applicant-card.blade.php
@@ -34,7 +34,7 @@
                 <a href="{{route('v2.employers.assessments.create', [$post->slug])}}" class="btn btn-success rounded-pill">Send Assessment</a>
             @endif --}}
             @if ($a->status == 'shortlisted')
-                <a href="/v2/employers/shortlist/{{ $post->slug }}/{{ $a->user->username }}" class="btn btn-primary ml-2 rounded-pill disabled">Shortlisted</a>
+                <a href="/v2/employers/shortlist/{{ $post->slug }}/{{ $a->user->username }}" class="btn btn-primary ml-2 rounded-pill">Remove from Shortlist</a>
             @else
                 <a href="/v2/employers/shortlist/{{ $post->slug }}/{{ $a->user->username }}" class="btn btn-primary ml-2 rounded-pill">Shortlist</a>
             @endif
