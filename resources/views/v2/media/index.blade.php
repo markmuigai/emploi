0e971e45bbdcb5df1f3c85a7f16d17b02cf9e7d1
kirarit <davidkirarit@gmail.com>
2020-12-15 12:02:05 +0300

    added two career videos

--- resources/views/v2/media/index.blade.php
+++ resources/views/v2/media/index.blade.php
@@ -13,28 +13,40 @@ Emploi is the Leading Platform for Talent Assessment and Matching for SME's in A
         <h4 class="orange" style="text-align: center;">Career Videos</h4>
         <div class="row">
             <div class="col-md-4 card">
+                 <iframe src="https://www.youtube.com/embed/shbQ6cJKqJw" frameborder="0" allowfullscreen></iframe>                    
+                    <h4>Welcome to Emploi 2.0: Learn about the website</h4>
+                    <p>Take a dive through the Emploi website and experience newly added features through this presentation.
+                    </p>                       
+            </div>
+            <div class="col-md-4 card">
+                 <iframe src="https://www.youtube.com/embed/oT1Xi8HS3FE" frameborder="0" allowfullscreen></iframe>                    
+                    <h4>Emploi Launch</h4>
+                    <p>Emploi 2.0 is finally here! Have a look at our new design, improve your job score ranking with our new Self-Assessment feature and gather insight on your resume with our AI- enabled Automatic CV Review. Stay tuned.
+                    </p>                       
+            </div>
+            <div class="col-md-4 card">
                  <iframe src="https://www.youtube.com/embed/7pj-dCfYptc" frameborder="0" allowfullscreen></iframe>                    
-                        <h4>Technology and Employment: Our CEO's interview with Ndereva Hillary of Y254 TV</h4>
-                        <p>Conversations around, the future of work, What is @emploi.co doing to help job seekers cope during this transition.?
-                        </p>                       
+                    <h4>Technology and Employment: Our CEO's interview with Ndereva Hillary of Y254 TV</h4>
+                    <p>Conversations around, the future of work, What is @emploi.co doing to help job seekers cope during this transition.?
+                    </p>                       
             </div>
             <div class="col-md-4 card">
                  <iframe src="https://www.youtube.com/embed/BVJ4punLoMc" frameborder="0" allowfullscreen></iframe>                    
-                        <h4><a href="/job-seekers/summit"> Resume Tips-Make Yourself a Stronger Candidate Today!</a></h4>
-                        <p>Here is Maggie giving insight into Emploi's CV editing service. LISTEN, SIGN UP AND GET HIRED TODAY.
-                        </p>                       
+                    <h4><a href="/job-seekers/summit"> Resume Tips-Make Yourself a Stronger Candidate Today!</a></h4>
+                    <p>Here is Maggie giving insight into Emploi's CV editing service. LISTEN, SIGN UP AND GET HIRED TODAY.
+                    </p>                       
             </div>
             <div class="col-md-4 card">
                  <iframe src="https://www.youtube.com/embed/E6dTmonX6Vk" frameborder="0" allowfullscreen></iframe>                    
-                        <h4><a href="/job-seekers/services">Emploi Services-Land that dream job Today!</a></h4>
-                        <p>Is your job search proving tough? Make it easier by signing up at Emploi.co today! 
-                        </p>                       
+                    <h4><a href="/job-seekers/services">Emploi Services-Land that dream job Today!</a></h4>
+                    <p>Is your job search proving tough? Make it easier by signing up at Emploi.co today! 
+                    </p>                       
             </div>
             <div class="col-md-4 card">
                  <iframe src="https://www.youtube.com/embed/fMkCW5yIYLs" frameborder="0" allowfullscreen></iframe>                   
-                        <h4>The future of jobs amid the job losses during COVID-19</h4>
-                        <p>A live conversation with CEO of Emploi, Sophy Mwale, and Simba Elijah Charles of Metropol TV on the future of jobs.
-                        </p>                       
+                    <h4>The future of jobs amid the job losses during COVID-19</h4>
+                    <p>A live conversation with CEO of Emploi, Sophy Mwale, and Simba Elijah Charles of Metropol TV on the future of jobs.
+                    </p>                       
             </div>
         </div>                      
     </div> 
