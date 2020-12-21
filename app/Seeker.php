cebfe719786885e7b1345a39c64ba7147907a206
kirarit <davidkirarit@gmail.com>
2020-12-21 16:59:59 +0300

    call scoreRefereeReport function on seeker model

--- app/Seeker.php
+++ app/Seeker.php
@@ -480,6 +480,13 @@ class Seeker extends Model
             $perc += 4;
         }
 
+        //referee feedback from assessment report
+            $referee_assessment=JobApplicationReferee::where('seeker_id', $this->id)->first();
+            if(isset($referee_assessment->id)){
+                //add the assessment score to his/her job score 
+                $perc += JobApplicationReferee::scoreRefereeReport($this->id);
+            }
+
         // if jobseeker has incomplete profile(cv and education level missing) or post has no model seeker
         if(!$this->hasCompletedProfile() || !$post->hasModelSeeker())
             //return $perc 
@@ -733,124 +740,7 @@ class Seeker extends Model
             $results = DB::select($sql);
             if(count($results) > 0)
                 $perc += $cosize;
-        }
-
-        
-        
-        
-        //referee feedback from assessment report
-        if(count($this->jobApplicationReferees) > 0)
-        {
-            $performance = array();
-            $workQuality = array();
-            $targets = array();
-            $rehire = array();
-            $discplinary = array();
-
-            for($i=0; $i<count($this->jobApplicationReferees); $i++)
-            {
-                $rf = $this->jobApplicationReferees[$i];
-                
-                    array_push($discplinary, $rf->discplinary_cases);
-                    array_push($rehire, $rf->would_you_rehire);
-            }
-
-            for($i=0; $i<count($this->seekerJobs); $i++)
-            {
-                if($this->seekerJobs[$i]->referee->status != 'ready')
-                    continue;
-                $sj = $this->seekerJobs[$i];
-                array_push($performance, $sj->work_performance);                
-                array_push($workQuality, $sj->work_quality);
-                array_push($targets, $sj->meeting_targets);
-            }
-            $performance = count($performance) > 0 ? array_sum($performance ) / count($performance) : 0;
-            $workQuality = count($workQuality) > 0 ? array_sum($workQuality ) / count($workQuality) : 0;
-            $targets = count($targets) > 0 ? array_sum($targets ) / count($targets) : 0;
-
-            //$perc += $ref;
-
-            //would you rehire
-            $hire = true;
-            //$nohire = false;
-            for($i=0; $i<count($rehire);$i++)
-            {
-                if($rehire[$i] == 'no' || $rehire[$i] == 'maybe')
-                    $hire = false;
-                // if($rehire[$i] == 'maybe')
-                //     $nohire = true;
-            }
-            if(!$hire)
-                $perc = $perc * 0.7;
-            // elseif($nohire)
-            //     $perc = $perc * 0.85;
-
-            //performance
-            if($performance >= 90)
-                $perc = $perc * 1.25;
-            elseif($performance>=75)
-                $perc = $perc * 1;
-            elseif($performance>=60)
-                $perc = $perc * 0.75;
-            elseif($performance>=50)
-                $perc = $perc * 0.5;
-            elseif($performance>=30)
-                $perc = $perc * 0.25;
-            else
-                $perc = $perc * 0.1;
-
-
-            //work quality
-            if($workQuality >= 90)
-                $perc = $perc * 1.25;
-            elseif($workQuality>=75)
-                $perc = $perc * 1;
-            elseif($workQuality>=60)
-                $perc = $perc * 0.75;
-            elseif($workQuality>=50)
-                $perc = $perc * 0.5;
-            elseif($workQuality>=30)
-                $perc = $perc * 0.25;
-            else
-                $perc = $perc * 0.1;
-
-            //ability to meet targets
-
-            if($targets >= 90)
-                $perc = $perc * 1.25;
-            elseif($targets>=75)
-                $perc = $perc * 1;
-            elseif($targets>=60)
-                $perc = $perc * 0.75;
-            elseif($targets>=50)
-                $perc = $perc * 0.5;
-            elseif($targets>=30)
-                $perc = $perc * 0.25;
-            else
-                $perc = $perc * 0.1;
-
-            //discplinary cases
-            $gross = false;
-            $mod = false;
-            $minor = false;
-            for($i=0; $i<count($discplinary);$i++)
-            {
-                if($discplinary[$i] == 'some')
-                    $mod = true;
-                if($discplinary[$i] == 'many')
-                    $gross = true;
-                if($discplinary[$i] == 'minor')
-                    $minor = true;
-            }
-            if($gross)
-                $perc = $perc * 0.5;
-            elseif($mod)
-                $perc = $perc * 0.85;
-            elseif($minor)
-                $perc = $perc * 0.95;
-
-            
-        }
+        }  
         return round($perc);
     }
 
