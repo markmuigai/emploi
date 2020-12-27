fde91372cf86a5b873bea6890a854f8566238646
kirarit <davidkirarit@gmail.com>
2020-12-27 16:10:03 +0300

    added self assessment on vacancy email

--- app/Seeker.php
+++ app/Seeker.php
@@ -860,13 +860,13 @@ class Seeker extends Model
     public function sendVacancyEmail($channel)     
 
     {       
-        $featured = Post::where('created_at', '>', Carbon::now()->subDays(100))
+        $featured = Post::where('created_at', '>', Carbon::now()->subDays(20))
                         ->where('status','active')
                         ->where('featured','true')
                         ->orderBy('id','DESC')
                         ->get();
 
-        $vacancies = Post::where('created_at', '>', Carbon::now()->subDays(100))
+        $vacancies = Post::where('created_at', '>', Carbon::now()->subDays(20))
                     ->where('industry_id',$this->industry_id)
                     ->where('status','active')
                     ->orderBy('created_at','DESC')
