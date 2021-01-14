6cf6bc3d20e16fecb8138bdf3cac6b62bca17b26
markmuigai <markmuigai@gmail.com>
2021-01-14 08:55:37 +0300

    Fetch evaluation result remark

--- app/EvaluationCriteriaResult.php
+++ app/EvaluationCriteriaResult.php
@@ -12,4 +12,36 @@ class EvaluationCriteriaResult extends Model
         'rating',
         'comment'
     ];
+
+    /**
+     * Get the associated criteria
+     */
+    public function criteria()
+    {
+        return $this->belongsTo('App\EvaluationCriteria', 'evaluation_criteria_id');
+    }
+
+    /**
+     * Get a results remark
+     */
+    public function remark()
+    {
+        switch($this->rating){
+            case 1 :
+                return 'Unsatisfactory';
+                break;
+            case 2 :
+                return 'Satisfactory';
+                break;
+            case 3 :
+                return 'Average';
+                break;
+            case 4 :
+                return 'Above Average';
+                break;
+            case 5 :
+                return 'Exceptional';
+                break;
+        }
+    }
 }
