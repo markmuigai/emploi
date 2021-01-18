6f073c8480790222ef9b326852c920f760e6f8a5
kirarit <davidkirarit@gmail.com>
2021-01-18 19:18:14 +0300

    save seeker profile

--- resources/views/v2/components/tables/employer/seekers.blade.php
+++ resources/views/v2/components/tables/employer/seekers.blade.php
@@ -45,12 +45,17 @@
                         </a>
                     @endif
 
-                    <button class="btn btn-success rounded-pill" data-toggle="tooltip" data-placement="top" title="Save profile">
+                 <!--    <button class="btn btn-success rounded-pill" data-toggle="tooltip" data-placement="top" title="Save profile">
                         <i class='bx bxs-heart' ></i>
-                    </button>
+                    </button> -->
+
+                    <a class="btn btn-success rounded-pill" type="button" data-toggle="modal" data-target="#saveSeekerModal-{{ $s->user->id }}" title="Save profile">
+                       <i class='bx bxs-heart'></i>
+                    </a>     
+
                 </td>
             </tr>
-            <!-- MODAL -->
+            <!--SHORTLIST MODAL -->
             <div class="modal fade" id="shortlistSeekerModal-{{ $s->user->id }}" tabindex="-1" role="dialog" aria-labelledby="shortlistSeekerModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -88,13 +93,51 @@
                                 </div>
                             </form>
                             @else
-                            <p class="text-warning">No job posted yet</p>
+                            <h5 class="text-danger text-center">No job posted yet</h5>
                             @endif
                         </div>
                     </div>
                 </div>
             </div>
-         <!--    END MODAL -->
+       <!--      END SHORTLIST SEEKER MODAL -->
+            
+    <!--         SAVE PROFILE MODAL -->
+             <div class="modal fade" id="saveSeekerModal-{{ $s->user->id }}" tabindex="-1" role="dialog" aria-labelledby="saveSeekerModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <h4 class="text-center mt-4">
+                           Save {{ $s->user->name }} profile?
+                        </h4>
+                            <div class="modal-body">
+                            <form method="post" action="/employers/saved" class="row">
+                                @csrf  
+                                <div class="job-filter-area pt-2">
+                                    <div class="container">
+                                        <form>
+                                            <div class="row">                              
+                                                <div class="col-sm-12 col-lg-12">
+                                                    <div class="form-group">
+                                                        <input type="hidden" name="seeker_id" value="{{ $s->id }}">                  
+                                                    
+                                                    </div>
+                                                </div>
+                              
+                                                <div class="col-sm-6 col-lg-12">
+                                                    <button type="submit" class="btn cmn-btn">
+                                                        Save
+                                                        <i class='bx bx-search-alt'></i>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>                                
+                                    </div>
+                                </div>
+                            </form>                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+         <!--    END SAVE PROFILE MODAL -->
             @empty
             @endforelse
         </tbody>
