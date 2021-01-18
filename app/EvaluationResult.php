0cc399f127cf9e3633338edb308c9a38029edf71
Mark Muigai <markmuigai@gmail.com>
2021-01-18 18:31:40 +0300

    Get an evaluations associated interview

--- app/EvaluationResult.php
+++ app/EvaluationResult.php
@@ -28,4 +28,12 @@ class EvaluationResult extends Model
     {
         return $this->hasMany('App\EvaluationCriteriaResult', 'evaluation_result_id');
     }
+
+    /**
+     * Get the associated interview
+     */
+    public function interview()
+    {
+        return $this->belongsTo('App\Interview');
+    }
 }
