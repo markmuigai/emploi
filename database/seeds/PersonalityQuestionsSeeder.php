beb9ccd6920b0663d0cd03685811a31af37872fd
Mark Muigai <markmuigai@gmail.com>
2021-01-26 17:52:52 +0300

    Sample personality questions seeder

---
+++ database/seeds/PersonalityQuestionsSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Question;
+use Illuminate\Database\Seeder;
+
+class PersonalityQuestionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $questions = [
+            'You enjoy vibrant social events with lots of people',
+            'You often spend time exploring unrealistic yet intriguing ideas',
+            'You enjoy vibrant social events with lots of people',
+            'People can rarely upset you',
+            'You often rely on other people to be the ones to start a conversation and keep it going',
+            'If you have to temporarily put your plans on hold, you make sure it is your top priority to get back on track as soon as possible',
+            'You are dedicated and focused on your goals, only rarely getting sidetracked',
+            'Your emotions control you more than you control them',
+            'You always consider how your actions might affect other people before doing something',
+            'I make friends easily'
+        ];
+
+        foreach($questions as $question){
+            $question = Question::create([
+                'title' => $question,
+                'type' => 'personality',
+                'difficulty_level' => 'easy'    
+            ]);
+            
+            $choices = ['Strongly Agree', 'Agree', 'Disagree', 'Strongly Disagree'];
+
+            foreach($choices as $choice){
+                // Create choices for each question
+                $question->choices()->create([
+                    'value' => $choice,
+                    'correct_value' => 1
+                ]);
+            }
+        }
+    }
+}
