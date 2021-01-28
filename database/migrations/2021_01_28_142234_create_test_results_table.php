9eb7731948d0b8fa69557e252bc4575dfcf73342
kirarit <davidkirarit@gmail.com>
2021-01-28 14:32:50 +0300

    test_result migration and model

---
+++ database/migrations/2021_01_28_142234_create_test_results_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_results', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->nullable();
+            $table->string('email');
+            $table->integer('assessment_count')->nullable();
+            $table->string('optional_message')->nullable();
+            $table->integer('score')->nullable();
+            $table->string('remarks')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_results');
+    }
+}
