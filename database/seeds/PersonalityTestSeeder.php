766d8e9d70e0a445b5687a3250e929fb1a23117f
Mark Muigai <markmuigai@gmail.com>
2021-01-29 17:56:53 +0300

    Updated personality test questions

--- database/seeds/PersonalityTestSeeder.php
+++ database/seeds/PersonalityTestSeeder.php
@@ -12,27 +12,66 @@ class PersonalityTestSeeder extends Seeder
      */
     public function run()
     {
+        // Delete all personality test questions and their choices
+        foreach(Question::where('type', 'personality')->get() as $question){
+            foreach($question->choices as $choice){
+                $choice->delete();
+            }
+
+            $question->delete();
+        }
+
         $questions = [
-            'I like to get on with my work without interference.',
-            'I go out of my way to make contact with new people.',
-            'I cannot feel relaxed unless I am certain I have made no mistakes.',
-            'I am frequently spontaneous in reacting to something that has happened.',
-            'I have troubles which weigh me down',
-            'The way I tackle a difficult problem is immediately.',
-            'It is up to people if they keep their thoughts to themselves, I still say what I think.',
-            'I feel happier when supporting rather than taking the lead myself.',
-            'I like to stick with my group.',
-            'I try to protect people by not saying hurtful things.',
-            'I do not take on more than can be done thoroughly and properly.',
-            'I tend to see the worst.',
-            'It often does not help to examine feelings too closely.',
-            'In the end, there’s not much that bothers me.',
-            'Teamwork gets the best from me.',
-            'I am a fairly quiet person.',
-            'I quickly get bored unless something exciting grabs my attention.',
-            'I am cool under pressure.',
-            'I rarely get emotional.',
-            'I like to be with lots of witty, amusing people.'
+            'Am the life of the party.',
+            'Feel little concern for others',
+            'Am always prepared',
+            'Get stressed out easily.',
+            'Have a rich vocabulary.',
+            "Don't talk a lot.",
+            'Am interested in people.',
+            'Leave my belongings around.',
+            'Am relaxed most of the time.',
+            'Have difficulty understanding abstract ideas.',
+            'Feel comfortable around people.',
+            'Insult people.',
+            'Pay attention to details.',
+            'Worry about things.',
+            'Have a vivid imagination.',
+            'Keep in the background',
+            "Sympathize with others' feelings",
+            'Make a mess of things',
+            'Seldom feel blue',
+            'Am not interested in abstract ideas',
+            'Start conversations.',
+            "Am not interested in other people's problems.",
+            'Get chores done right away.',
+            'Am easily disturbed.',
+            'Have excellent ideas.',
+            'Have little to say.',
+            'Have a soft heart.',
+            'Often forget to put things back in their proper place.',
+            'Get upset easily.',
+            'Do not have a good imagination.',
+            'Talk to a lot of different people at parties.',
+            'Am not really interested in others.',
+            'Like order.',
+            'Change my mood a lot.',
+            'Am quick to understand things.',
+            "Don't like to draw attention to myself.",
+            'Take time out for others.',
+            'Shirk my duties',
+            'Have frequent mood swings',
+            'Use difficult words.',
+            "Don't mind being the center of attention.",
+            "Feel others' emotions.",
+            'Follow a schedule',
+            'Get irritated easily.',
+            'Spend time reflecting on things.',
+            'Am quiet around strangers.',
+            'Make people feel at ease.',
+            'Am exacting in my work.',
+            'Often feel blue.',
+            'Am full of ideas.',
         ];
 
         foreach($questions as $question){
@@ -42,7 +81,7 @@ class PersonalityTestSeeder extends Seeder
                 'difficulty_level' => 'easy'    
             ]);
             
-            $choices = ['Strongly Agree', 'Agree', 'Disagree', 'Strongly Disagree'];
+            $choices = ['Disagree', 'Slightly Disagree', 'Neutral', 'Slightly Agree', 'Agree'];
 
             foreach($choices as $choice){
                 // Create choices for each question
