58fcd63dd1901f3384b70ba3687ee5c49e54e8a8
kirarit <davidkirarit@gmail.com>
2021-02-01 14:08:15 +0300

    cv review filter by date

--- app/Http/Controllers/Admin/CVReviewController.php
+++ app/Http/Controllers/Admin/CVReviewController.php
@@ -19,39 +19,26 @@ class CVReviewController extends Controller
         if(isset($request->sortbydate))
             switch ($request->sortbydate) {
                 case 'today':
-                $sort_by_date =CVReviewResult::where('created_at', '>', Carbon::now()->subDays(1))->pluck('user_id')->toArray();
-                    // return $sort_by_date;
+                $cvReview =CVReviewResult::where('created_at', '>', Carbon::now()->subDays(1))->with('recommendations')->orderBy('id','DESC');
                     break;
 
                 case 'last7':
-                    $sort_by_date =CVReviewResult::where('created_at', '>', Carbon::now()->subDays(7))->pluck('user_id')->toArray();
-                    // return $sort_by_date;
+                    $cvReview =CVReviewResult::where('created_at', '>', Carbon::now()->subDays(7))->with('recommendations')->orderBy('id','DESC');
                     break;
                 case 'thisMonth':
-                    $sort_by_date =CVReviewResult::where('created_at', '>', Carbon::now()->subDays(30))->pluck('user_id')->toArray();
-                    // return $sort_by_date;
+                    $cvReview =CVReviewResult::where('created_at', '>', Carbon::now()->subDays(30))->with('recommendations')->orderBy('id','DESC');
                     break;
                 
                 default:
                     break;
 
+      }else{
+        $cvReview = CVReviewResult::with('recommendations')->orderBy('id','DESC');
       } 
-
-      if(isset($request->sortbydate)){
-          return view('v2.admin.cvReview.index',[
-                'cvReviews' => CVReviewResult::with('recommendations')->whereIn('user_id',$sort_by_date)->orderBy('id','DESC')->paginate(15),
-                'count' => CVReviewResult::all()->count(),
-                'avg' => ceil(CVReviewResult::all()->pluck('score')->avg()),
-                'missingKeyword' => CVReviewResult::missingKeyword(),
-                'convertedEmails' => CVReviewResult::convertedEmails()
-            ]);
-          }
-
-          //all cv review results
         return view('v2.admin.cvReview.index',[
-            'cvReviews' => CVReviewResult::with('recommendations')->orderBy('id','DESC')->paginate(15),
-            'count' => CVReviewResult::all()->count(),
-            'avg' => ceil(CVReviewResult::all()->pluck('score')->avg()),
+            'cvReviews' => $cvReview->paginate(15),
+            'count' => $cvReview->count(),
+            'avg' => ceil($cvReview->pluck('score')->avg()),
             'missingKeyword' => CVReviewResult::missingKeyword(),
             'convertedEmails' => CVReviewResult::convertedEmails()
         ]);
