0042adb0bf4711fcc5030715a8f3aaac3e1e327b
kirarit <davidkirarit@gmail.com>
2021-02-08 22:46:46 +0300

    newusers seeder ->whereBetween('id',[20001, 25000])

--- database/seeds/NewUsersSeeder.php
+++ database/seeds/NewUsersSeeder.php
@@ -19,7 +19,7 @@ class NewUsersSeeder extends Seeder
         DB::transaction(function () {
  
             $users = DB::table('unregistered_users')
-			->whereBetween('id',[15001, 20000])
+			->whereBetween('id',[20001, 25000])
 			->select('email')->get();     
 
 
