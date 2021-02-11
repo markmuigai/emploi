377a2ec4ebdcf7cdd1e88cb69be811cdb169c353
kirarit <davidkirarit@gmail.com>
2021-02-11 14:03:05 +0300

    added job score on seeker-card blade

--- resources/views/v2/components/employer/seeker-card.blade.php
+++ resources/views/v2/components/employer/seeker-card.blade.php
@@ -12,6 +12,9 @@
                 <a href="/employers/browse/{{ $s->user->username }}" target="_blank">{{ $s->user->seeker->public_name }}</a>
             @endif
         </h3>
+        @if(isset(request()->post))
+            <h6>Job suitability score: {{ $s->user->seeker->calculateRsi($post) }}%</h6>
+        @endif
         <div class="row">
             <div class="col-md-7">
                 @if($s->user->seeker->featured > 0)
