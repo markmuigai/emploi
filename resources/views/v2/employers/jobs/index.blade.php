af99e2e05fece97a7dbd741405f2944e85d773f6
markmuigai <markmuigai@gmail.com>
2021-02-15 13:03:20 +0300

    Added job category nav pills

--- resources/views/v2/employers/jobs/index.blade.php
+++ resources/views/v2/employers/jobs/index.blade.php
@@ -16,10 +16,12 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
     <div class="job-area-list dashboard-area mt-1 ptb-100">
         <div class="container-fluid px-2">
             <div class="row">
-                <div class="col-lg-2 pt-3">
+                <div class="col-lg-2 pt-3 position-fixed">
+                    <div class="affix">
                     @include('v2.components.sidebar.employer')               
+                    </div>
                 </div>           
-                 <div class="col-lg-10 jobs-form">
+                 <div class="col-lg-10 jobs-form offset-md-2">
                     <div class="categories-area">
                         <div class="container-fluid mb-5">
                             <h3 class="my-3 text-center">Employer Dashboard</h3>
@@ -100,46 +102,61 @@ Emploi is the Leading Platform for Recruitment and Placement Solutions for SMEs 
                         </div>
                     </div>
                     <div class="candidate-area pb-100">
-                      <h4 class="mb-4 text-center">Jobs</h4>
-                      <div class="row">
-                          @foreach ($posts as $post)
-                          <div class="col-lg-3">
-                              <div class="candidate-item two">
-                                  <div class="job left">
-                                      <h3 class="text-center">
-                                        <a href="/v2/employers/jobs/{{ $post->slug }}/applications">{{ $post->title }}</a>
-                                      </h3>
-                                      <ul class="experience">
-                                          <li>Closing: <i class="far fa-calendar-check"></i> {{ $post->readableDeadline }}</li>
-                                          <li>Salary: <strong>{{ $post->monthlySalary() }} p.m.</strong></li>
-                                          @if($post->how_to_apply)
-                                            <li>Alternative Application</li>
-                                          @else
-                                            <li>No. of Applicants: {{ count($post->applications) }}</li>
-                                          @endif
-                                      </ul>
-                                      <a href="/vacancies/{{ $post->slug }}/edit" class="btn btn-success  btn-sm">
-                                        Edit Job
-                                        <i class='bx bxs-pencil'></i>
-                                      </a>
-                                      @if($post->status == 'active')
-                                        <a href="/vacancies/laravel-developer/deactivate" class="btn btn-danger  btn-sm">
-                                          Deactivate
-                                          <i class='bx bxs-trash' ></i>
-                                        </a>
-                                      @elseif($post->status == 'closed')
-                                        <a href="/vacancies/laravel-developer/activate" class="btn btn-success"><i class="far fa-eye"></i> Activate</a>
-                                      @else
-                                        <p class="text-danger">Not Verified</p>
-                                      @endif
-                                  </div>
-                              </div>
-                          </div>
-                          @endforeach                             
-                      </div>
-                      <div class="text-center">
-                          {{ $posts->links() }}
-                      </div>
+											<div class="container-fluid">
+												<ul class="nav nav-pills employer mb-3" id="pills-tab" role="tablist">
+													<li class="nav-item">
+														<a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">All jobs</a>
+													</li>
+													<li class="nav-item">
+														<a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Shortlisting</a>
+													</li>
+													<li class="nav-item">
+														<a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Active Jobs</a>
+													</li>
+													<li class="nav-item">
+													<a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Other Jobs</a>
+														</li>
+												</ul>
+												<div class="row">
+														@foreach ($posts as $post)
+														<div class="col-lg-3">
+																<div class="candidate-item two">
+																		<div class="job left">
+																				<h3 class="text-center">
+																					<a href="/v2/employers/jobs/{{ $post->slug }}/applications">{{ $post->title }}</a>
+																				</h3>
+																				<ul class="experience">
+																						<li>Closing: <i class="far fa-calendar-check"></i> {{ $post->readableDeadline }}</li>
+																						<li>Salary: <strong>{{ $post->monthlySalary() }} p.m.</strong></li>
+																						@if($post->how_to_apply)
+																							<li>Alternative Application</li>
+																						@else
+																							<li>No. of Applicants: {{ count($post->applications) }}</li>
+																						@endif
+																				</ul>
+																				<a href="/vacancies/{{ $post->slug }}/edit" class="btn btn-success  btn-sm">
+																					Edit Job
+																					<i class='bx bxs-pencil'></i>
+																				</a>
+																				@if($post->status == 'active')
+																					<a href="/vacancies/laravel-developer/deactivate" class="btn btn-danger  btn-sm">
+																						Deactivate
+																						<i class='bx bxs-trash' ></i>
+																					</a>
+																				@elseif($post->status == 'closed')
+																					<a href="/vacancies/laravel-developer/activate" class="btn btn-success"><i class="far fa-eye"></i> Activate</a>
+																				@else
+																					<p class="text-danger">Not Verified</p>
+																				@endif
+																		</div>
+																</div>
+														</div>
+														@endforeach                             
+												</div>
+												<div class="text-center">
+														{{ $posts->links() }}
+												</div>
+											</div>
                     </div>
                 </div>
             </div>
