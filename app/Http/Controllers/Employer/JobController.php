d82f6a2d2f2ddd98f5ae94bf6d47e22a72a61340
markmuigai <markmuigai@gmail.com>
2021-02-17 09:32:00 +0300

    Replaced collections with eloquent in fetch job by category

--- app/Http/Controllers/Employer/JobController.php
+++ app/Http/Controllers/Employer/JobController.php
@@ -8,109 +8,97 @@ use App\Http\Controllers\Controller;
 
 class JobController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
-     */
-    public function index()
-    {
-        $employer = auth()->user()->employer;
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+	 */
+	public function index()
+	{
+		$employer = auth()->user()->employer;
+		$posts = $employer->posts;
 
-        $companies = $employer->user->companies;
+		if (request()->job_category == 'shortlisting') {
+			$posts = $employer->shortlistingPosts;
+		}
 
-        // Get posts associated with the companies of the employer
-        $posts = $companies->map(function($company){
-            return $company->posts;
-        })->flatten()->sortByDesc('id');
+		if (request()->job_category == 'active') {
+			$posts = $employer->activePosts;
+		}
 
-        if(request()->job_category == 'shortlisting'){
-            $posts = $posts->filter(function($post){
-                return $post->how_to_apply == null;
-            });
-        }
+		if (request()->job_category == 'other') {
+			$posts = $employer->closedPosts;
+		}
 
-        if(request()->job_category == 'active'){
-            $posts = $posts->filter(function($post){
-                return $post->status == 'active';
-            });
-        }
+		return view('v2.employers.jobs.index', [
+			'posts' => CollectionHelper::paginate($posts, 10),
+			'recentApplications' => $employer->recentApplications()
+		]);
+	}
 
-        if(request()->job_category == 'other'){
-            $posts = $posts->filter(function($post){
-                return $post->status != 'active';
-            });
-        }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//
+	}
 
-        return view('v2.employers.jobs.index',[
-            'posts' => CollectionHelper::paginate($posts, 10),
-            'recentApplications' => $employer->recentApplications()
-        ]);
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
