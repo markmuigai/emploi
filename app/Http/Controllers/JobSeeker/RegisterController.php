3443f575424e75c0f0004952997192856cf7a6c2
kirarit <davidkirarit@gmail.com>
2021-02-18 11:54:01 +0300

    jobseeker register controller

---
+++ app/Http/Controllers/JobSeeker/RegisterController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\JobSeeker;
+
+use Storage;
+use App\Parser;
+use App\Country;
+use App\Industry;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('v2.seekers.register.create')
+            ->with('countries',Country::all())
+            ->with('industries',Industry::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       $request->validate([
+                'resume'  =>  ['mimes:doc,pdf,docx','max:51200']
+            ]);
+
+        $storage_path = '/public/resumes';
+        $resume_url = basename(Storage::put($storage_path, $request->resume));
+        $parser = new Parser($resume_url);
+        $contents = $parser->convertToText();
+        dd($contents);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
